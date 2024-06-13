4ad92c32402d9d7e0e0adbdb06cbb22fc7781e43
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-06-13 08:07:41 -0400

    feat(exception): wip

---
+++ engine/src/api/interfaces/IException.ts
@@ -0,0 +1,31 @@
+import { IReadOnlyDictionary } from '@api/interfaces/IReadOnlyDictionary.js'
+
+export enum ExceptionDictionaryName {
+  type = 'type',
+  name = 'name',
+  message = 'message',
+  stack = 'stack'
+}
+
+export enum ExceptionType {
+  system = 'system',
+  custom = 'custom'
+}
+
+export enum SystemExceptionName {
+  break = 'break',
+  busy = 'busy',
+  dictStackUnderflow = 'dictStackUnderflow',
+  invalidAccess = 'invalidAccess',
+  invalidBreak = 'invalidBreak',
+  rangeCheck = 'rangeCheck',
+  stackUnderflow = 'stackUnderflow',
+  typeCheck = 'typeCheck',
+  undefined = 'undefined',
+  unmatchedMark = 'unmatchedMark',
+  vmError = 'vmError'
+}
+
+export interface IException extends Error, IReadOnlyDictionary {
+  readonly type: ExceptionType
+}
---
+++ engine/src/sdk/exceptions/BaseException.ts
@@ -0,0 +1,58 @@
+import type { IReadOnlyDictionary, ISharedReference, Value, ValueType } from '@api/index'
+
+export class BaseError extends Error implements IReadOnlyDictionary, ISharedReference {
+    constructor (message: string, name?: string) {
+      super(message)
+      this.name = name ?? this.constructor.name
+    }
+  
+    get dictionary (): IReadOnlyDictionary {
+      return this
+    }
+  
+    release (): void {}
+  
+    private _callstack: string = ''
+  
+    get callstack (): string {
+      if (this._callstack !== '') {
+        return this._callstack
+      }
+      return this.stack?.split(CR).slice(1).join(CR) ?? NO_STACK
+    }
+  
+    set callstack (value: string) {
+      if (this._callstack === '') {
+        this._callstack = value
+      }
+    }
+  
+    // region IDictionary
+  
+    get names (): string[] {
+      return NAMES
+    }
+  
+    lookup (name: string): Value | null {
+      let string: string | undefined
+      if (name === TYPE_PROPERTY) {
+        string = 'system'
+      } else if (name === NAME_PROPERTY) {
+        string = this.name
+      } else if (name === MESSAGE_PROPERTY) {
+        string = this.message
+      } else if (name === STACK_PROPERTY) {
+        string = this.callstack
+      }
+      if (string !== undefined) {
+        return {
+          type: ValueType.string,
+          string
+        }
+      }
+      return null
+    }
+  
+    // endregion IDictionary
+  }
+  
