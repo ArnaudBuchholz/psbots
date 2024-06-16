a3368ef524912728f9677edf64e08dedb8b2fd7b
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-06-16 00:26:22 -0400

    fix: linter

--- tools/sync-aliases.mjs
+++ tools/sync-aliases.mjs
@@ -1,18 +1,20 @@
-import { readFile, writeFile } from 'node:fs/promises'
+import { readFile, writeFile } from 'node:fs/promises';
 
 const main = async () => {
-  const { compilerOptions: { paths: aliases }} = JSON.parse(await readFile('tsconfig.json', 'utf-8'))
+  const {
+    compilerOptions: { paths: aliases }
+  } = JSON.parse(await readFile('tsconfig.json', 'utf-8'));
   await writeFile(
     'vite.config.mjs',
-    (await readFile('vite.config.mjs', 'utf-8'))
-    .replace(/alias: \{[^}]*}/, () => `alias: {
-      ${
-        Object.entries(aliases)
-          .map(([alias, path]) => `'${alias.split('/*')[0]}': path('src/${path[0].split('/*')[0]}')`)
-          .join(',\n      ')
-      }
-    }`)    
-  )
-}
+    (await readFile('vite.config.mjs', 'utf-8')).replace(
+      /alias: \{[^}]*}/,
+      () => `alias: {
+      ${Object.entries(aliases)
+        .map(([alias, path]) => `'${alias.split('/*')[0]}': path('src/${path[0].split('/*')[0]}')`)
+        .join(',\n      ')}
+    }`
+    )
+  );
+};
 
-main().catch(reason => console.error(reason))
+main().catch((reason) => console.error(reason));
