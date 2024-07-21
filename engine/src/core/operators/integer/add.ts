f55ab6c6aec084db0599f52a87626efb105ed02a
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-07-21 11:02:24 -0400

    chores(operators): no need to document implementation

--- engine/src/core/operators/integer/add.ts
+++ engine/src/core/operators/integer/add.ts
@@ -1,3 +1,4 @@
+// TODO provide a basic implementation wrapper where signature decides input & output
 export function addImpl(value1: number, value2: number): number {
   return value1 + value2;
 }
