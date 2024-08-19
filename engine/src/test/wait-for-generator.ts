c5150d3b2557db77ca5fef26fe87df54f9221446
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-08-18 23:14:12 -0400

    fix: linter

--- engine/src/test/wait-for-generator.ts
+++ engine/src/test/wait-for-generator.ts
@@ -1,14 +1,15 @@
-export function waitForGenerator (iterator: Generator): any[] {
-  const result = []
+export function waitForGenerator(iterator: Generator): unknown[] {
+  const result = [];
+  // eslint-disable-next-line no-constant-condition
   while (true) {
-    const { value, done } = iterator.next()
-    result.push(value)
+    const { value, done } = iterator.next();
+    result.push(value);
     if (result.length > 1000) {
-      throw new Error('Too many cycles (infinite loop ?)')
+      throw new Error('Too many cycles (infinite loop ?)');
     }
     if (done === true) {
-      break
+      break;
     }
   }
-  return result
+  return result;
 }
