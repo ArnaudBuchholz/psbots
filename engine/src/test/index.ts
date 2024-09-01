e76e60d65bda207e77cb4029ddf684b43dc5b38a
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-09-01 15:19:11 -0400

    refactor: remove useless file

--- engine/src/test/index.ts
+++ engine/src/test/index.ts
@@ -1,6 +1,5 @@
 export * from './check-function.js';
 export * from './enumVariantsOf.js';
-export * from './operator.js';
 export * from './stringify.js';
 export * from './toValue.js';
 export * from './values.js';
--- engine/src/test/operator.ts
+++
@@ -1,5 +0,0 @@
-import { it, expect } from 'vitest';
-
-export function testOperator(path: string): void {
-  it(`operator ${path}`, () => expect(true).toBe(true));
-}
