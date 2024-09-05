0f45475fdb7779dfeea17d614776559323511fd9
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-09-05 09:46:38 -0400

    feat: cli using module

--- cli/index.js
+++ cli/index.js
@@ -1,11 +1,10 @@
-import type { IReplIO } from '@psbots/repl';
 import { repl } from '@psbots/repl';
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 
 const rl = readline.createInterface({ input, output });
 
-const replIO: IReplIO = {
+repl({
   width: process.stdout.columns,
   height: process.stdout.rows,
   output(text) {
@@ -14,9 +13,7 @@ const replIO: IReplIO = {
   async input() {
     return await rl.question('? ');
   }
-};
-
-repl(replIO).catch((reason) => {
+}).catch((reason) => {
   console.error(reason);
   process.exitCode = -1;
 });
