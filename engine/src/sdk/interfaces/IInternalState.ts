93c10b0f6363e7aaf42a3aab2cbce51932a848ec
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-09-07 00:29:10 -0400

    fix: remove duplicate member

--- engine/src/sdk/interfaces/IInternalState.ts
+++ engine/src/sdk/interfaces/IInternalState.ts
@@ -1,11 +1,10 @@
-import type { IMemoryTracker, IState } from '@api/index.js';
+import type { IState } from '@api/index.js';
 import type { ICallStack } from '@sdk/interfaces/ICallStack.js';
 import type { IDictionaryStack } from '@sdk/interfaces/IDictionaryStack.js';
 import type { IStack } from '@sdk/interfaces/IStack.js';
 import type { BaseException } from '@sdk/exceptions/BaseException.js';
 
 export interface IInternalState extends IState {
-  readonly memoryTracker: IMemoryTracker;
   readonly operands: IStack;
   readonly dictionaries: IDictionaryStack;
   readonly calls: ICallStack;
