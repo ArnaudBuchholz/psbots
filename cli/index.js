0e44d0f8a6079cfc629b964e799b66e072a3e455
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-09-19 16:05:17 -0400

    feat(cli): adds debug option

--- cli/index.js
+++ cli/index.js
@@ -4,16 +4,19 @@ import { stdin as input, stdout as output } from 'node:process';
 
 const rl = readline.createInterface({ input, output });
 
-await repl({
-  width: process.stdout.columns,
-  height: process.stdout.rows,
-  output(text) {
-    console.log(text + '\x1b[37m');
+await repl(
+  {
+    width: process.stdout.columns,
+    height: process.stdout.rows,
+    output(text) {
+      console.log(text + '\x1b[37m');
+    },
+    async input() {
+      return await rl.question('? ');
+    }
   },
-  async input() {
-    return await rl.question('? ');
-  }
-}).catch((reason) => {
+  process.argv.includes('--debug')
+).catch((reason) => {
   console.error(reason);
   process.exitCode = -1;
 });
