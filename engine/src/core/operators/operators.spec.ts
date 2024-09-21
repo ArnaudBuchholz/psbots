13bf222115753a6a85d78e6098b8d2eb707d7846
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-09-21 13:45:44 -0400

    test(operators): evaluate performances

--- engine/src/core/operators/operators.spec.ts
+++ engine/src/core/operators/operators.spec.ts
@@ -16,56 +16,125 @@ const nullDefinition: OperatorDefinition = {
   samples: []
 };
 
-Object.keys(registry)
-  .sort()
-  .forEach((operatorName) => {
-    describe(`operators/${operatorName}`, () => {
-      const definition: OperatorDefinition = registry[operatorName]?.definition ?? nullDefinition;
-      let state: State;
-      let expectedState: State;
+let skipped = 0;
+let debugCycles = 0;
+let debugMilliseconds = 0;
 
-      beforeEach(() => {
-        state = new State({ debugMemory: true });
-        expectedState = new State({ debugMemory: true });
-      });
+describe('executing in & out using debug', () => {
+  Object.keys(registry)
+    .sort()
+    .forEach((operatorName) => {
+      describe(`operators/${operatorName})`, () => {
+        const definition: OperatorDefinition = registry[operatorName]?.definition ?? nullDefinition;
+        let state: State;
+        let expectedState: State;
 
-      afterEach(() => {
-        expectedState.destroy();
-        state.destroy();
-      });
+        beforeEach(() => {
+          state = new State({ debugMemory: true });
+          expectedState = new State({ debugMemory: true });
+        });
 
-      it('exposes a definition', () => {
-        expect(definition).not.toStrictEqual(nullDefinition);
-      });
+        afterEach(() => {
+          expectedState.destroy();
+          state.destroy();
+        });
 
-      definition.samples.forEach((sample, index) => {
-        const missingOperators = [...parse(sample.in), ...parse(sample.out)]
-          .filter((value) => value.type === 'string' && value.isExecutable)
-          .map((value) => (value.type === 'string' ? value.string : ''))
-          .filter((name) => !Object.prototype.hasOwnProperty.call(registry, name) && !name.startsWith('test'))
-          .reduce((names, name) => (names.includes(name) ? names : [name, ...names]), <string[]>[]);
-        const sampleId = `${operatorName}#${index}`;
-        const description = sample.description ?? definition.description;
-        if (missingOperators.length > 0) {
-          it.skip(`[${sampleId}] ${description} (⚠️ ${missingOperators})`);
-        } else {
-          it(`[${sampleId}] ${description}`, () => {
-            waitForGenerator(state.process(sample.in));
-            waitForGenerator(expectedState.process(sample.out));
-            if (expectedState.exception) {
-              expect(state.exception).not.toBeUndefined();
-              expect(state.exception).toBeInstanceOf(expectedState.exception.constructor);
-            } else {
-              expect(state.exception).toBeUndefined();
-              expect(state.operands.ref.length).toStrictEqual(expectedState.operands.ref.length);
-              // flatten differences between the two memory trackers
-              Object.assign(state.memoryTracker, { _peak: 0 });
-              Object.assign(expectedState.memoryTracker, { _peak: 0 });
-              expect(state.memoryTracker.byType).toStrictEqual(expectedState.memoryTracker.byType);
-              expect(state.operands.ref).toStrictEqual(expectedState.operands.ref);
-            }
-          });
-        }
+        it('exposes a definition', () => {
+          expect(definition).not.toStrictEqual(nullDefinition);
+        });
+
+        definition.samples.forEach((sample, index) => {
+          const missingOperators = [...parse(sample.in), ...parse(sample.out)]
+            .filter((value) => value.type === 'string' && value.isExecutable)
+            .map((value) => (value.type === 'string' ? value.string : ''))
+            .filter((name) => !Object.prototype.hasOwnProperty.call(registry, name) && !name.startsWith('test'))
+            .reduce((names, name) => (names.includes(name) ? names : [name, ...names]), <string[]>[]);
+          const sampleId = `${operatorName}#${index}`;
+          const description = sample.description ?? definition.description;
+          if (missingOperators.length > 0) {
+            it.skip(`[${sampleId}] ${description} (⚠️ ${missingOperators})`);
+            ++skipped;
+          } else {
+            it(`[${sampleId}] ${description}`, () => {
+              const start = performance.now();
+              debugCycles += waitForGenerator(state.process(sample.in)).length;
+              debugCycles += waitForGenerator(expectedState.process(sample.out)).length;
+              debugMilliseconds += Math.ceil(performance.now() - start);
+              if (expectedState.exception) {
+                expect(state.exception).not.toBeUndefined();
+                expect(state.exception).toBeInstanceOf(expectedState.exception.constructor);
+              } else {
+                expect(state.exception).toBeUndefined();
+                expect(state.operands.ref.length).toStrictEqual(expectedState.operands.ref.length);
+                // flatten differences between the two memory trackers
+                Object.assign(state.memoryTracker, { _peak: 0 });
+                Object.assign(expectedState.memoryTracker, { _peak: 0 });
+                expect(state.memoryTracker.byType).toStrictEqual(expectedState.memoryTracker.byType);
+                expect(state.operands.ref).toStrictEqual(expectedState.operands.ref);
+              }
+            });
+          }
+        });
       });
     });
+});
+
+let cycles = 0;
+let milliseconds = 0;
+
+describe('executing in only (performance)', () => {
+  Object.keys(registry)
+    .sort()
+    .forEach((operatorName) => {
+      describe(`operators/${operatorName}`, () => {
+        const definition: OperatorDefinition = registry[operatorName]?.definition ?? nullDefinition;
+        let state: State;
+
+        beforeEach(() => {
+          state = new State();
+        });
+
+        afterEach(() => {
+          state.destroy();
+        });
+
+        definition.samples.forEach((sample, index) => {
+          const missingOperators = [...parse(sample.in), ...parse(sample.out)]
+            .filter((value) => value.type === 'string' && value.isExecutable)
+            .map((value) => (value.type === 'string' ? value.string : ''))
+            .filter((name) => !Object.prototype.hasOwnProperty.call(registry, name) && !name.startsWith('test'))
+            .reduce((names, name) => (names.includes(name) ? names : [name, ...names]), <string[]>[]);
+          const sampleId = `${operatorName}#${index}`;
+          const description = sample.description ?? definition.description;
+          if (missingOperators.length === 0) {
+            it(`[${sampleId}] ${description}`, () => {
+              const start = performance.now();
+              cycles += waitForGenerator(state.process(sample.in)).length;
+              milliseconds += Math.ceil(performance.now() - start);
+            });
+          }
+        });
+      });
+    });
+});
+
+describe('Execution report for operators', () => {
+  if (skipped === 1) {
+    it.skip('1 test is skipped');
+  } else if (skipped > 0) {
+    it.skip(`${skipped} tests are skipped`);
+  }
+
+  it('(debug) cycles/ms > 0', () => {
+    const debugRatio = Math.floor((100 * debugCycles) / debugMilliseconds) / 100;
+    console.log('(debug) cycles/ms', debugRatio);
+    expect(debugRatio).toBeGreaterThan(0);
   });
+
+  it('cycles/ms > (debug) cycles/ms', () => {
+    const debugRatio = Math.floor((100 * debugCycles) / debugMilliseconds) / 100;
+    const ratio = Math.floor((100 * cycles) / milliseconds) / 100;
+    console.log('cycles/ms', ratio);
+    expect(ratio).toBeGreaterThan(debugRatio);
+  });
+});
