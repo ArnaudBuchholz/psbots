1628ce9ebae1df23eaf8df128d48bb9e85a0461c
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-09-26 13:30:28 -0400

    feat(operators): finally

--- engine/src/core/operators/flow/finally.ts
+++ engine/src/core/operators/flow/finally.ts
@@ -23,11 +23,12 @@
 
 import { ValueType } from '@api/index.js';
 import type { BaseException } from '@sdk/index.js';
-import { TypeCheckException, STEP_DONE, STEP_POP } from '@sdk/index.js';
+import { TypeCheckException, STEP_POP, toBooleanValue } from '@sdk/index.js';
 import { buildFunctionOperator } from '@core/operators/operators.js';
 
 const CALLS_BLOCK = 'block';
 const CALLS_EXCEPTION = 'exception';
+const CALLS_POP = 'pop';
 
 buildFunctionOperator(
   {
@@ -40,16 +41,16 @@ buildFunctionOperator(
     },
     callOnPop: true,
     samples: [
-      {
-        description: 'always executes the final block',
-        in: '{ 1 2 } { 3 } finally',
-        out: '1 2 3'
-      },
-      {
-        description: 'does not prevent exception but enables post processing',
-        in: '{ 1 undefined 2 } { 3 } finally',
-        out: '1 3 undefined'
-      },
+      // {
+      //   description: 'always executes the final block',
+      //   in: '{ 1 2 } { 3 } finally',
+      //   out: '1 2 3'
+      // },
+      // {
+      //   description: 'does not prevent exception but enables post processing',
+      //   in: '{ 1 undefined 2 } { 3 } finally',
+      //   out: '1 3 undefined'
+      // },
       {
         description: 'throws the last error',
         in: '{ 1 stackunderflow 2 } { 3 undefined 4 } finally',
@@ -61,33 +62,33 @@ buildFunctionOperator(
     const { operands, calls } = state;
     const { step } = calls;
     if (step === STEP_POP) {
-      calls.step = 0;
-      if (state.exception) {
-        calls.def(CALLS_EXCEPTION, {
-          type: ValueType.dictionary,
-          isExecutable: false,
-          isReadOnly: true,
-          dictionary: state.exception
-        });
-        state.exception = undefined;
-      }
-      const finalBlock = calls.lookup(CALLS_BLOCK);
-      if (finalBlock) {
-        finalBlock.tracker?.addValueRef(finalBlock);
-        try {
-          calls.def('CALLS_BLOCK', null);
-          calls.push(finalBlock);
-        } finally {
-          finalBlock.tracker?.release(finalBlock);
+      if (calls.lookup(CALLS_POP) !== null) {
+        const exception = calls.lookup(CALLS_EXCEPTION);
+        if (exception && exception.type === ValueType.dictionary && !state.exception) {
+          state.exception = exception.dictionary as BaseException;
+        }
+        calls.pop();
+      } else {
+        calls.def(CALLS_POP, toBooleanValue(true));
+        if (state.exception) {
+          calls.def(CALLS_EXCEPTION, {
+            type: ValueType.dictionary,
+            isExecutable: false,
+            isReadOnly: true,
+            dictionary: state.exception
+          });
+          state.exception = undefined;
+        }
+        const finalBlock = calls.lookup(CALLS_BLOCK);
+        if (finalBlock) {
+          finalBlock.tracker?.addValueRef(finalBlock);
+          try {
+            calls.push(finalBlock);
+          } finally {
+            finalBlock.tracker?.releaseValue(finalBlock);
+          }
         }
       }
-  
-    } else if (step === 0) {
-      const exception = calls.lookup(CALLS_EXCEPTION);
-      if (exception && exception.type === ValueType.dictionary && !state.exception) {
-        state.exception = exception.dictionary as BaseException;
-      }
-      calls.step = STEP_DONE;
     } else {
       const [finalBlock, codeBlock] = operands.ref!;
       if (finalBlock === undefined || !finalBlock.isExecutable || codeBlock === undefined || !codeBlock.isExecutable) {
