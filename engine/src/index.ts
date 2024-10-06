6e801633d31dae5757be6a0e0ac783a341ce43d0
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-10-06 13:23:54 -0400

    feat: exposes OperatorDefinitionRegistry

--- engine/src/index.ts
+++ engine/src/index.ts
@@ -1,5 +1,7 @@
 import type { IState } from '@api/interfaces/IState.js';
+import type { OperatorDefinition } from '@core/operators/operators.js';
 import type { StateFactorySettings } from '@core/state/State.js';
+import { registry } from '@core/operators/operators.js';
 import { State } from '@core/state/State.js';
 
 export { StateFactorySettings } from '@core/state/State.js';
@@ -8,4 +10,15 @@ export * from './api/index.js';
 /** State factory */
 export function createState(settings?: StateFactorySettings): IState {
   return new State(settings);
+}
+
+/** Operators registry */
+export function getOperatorDefinitionRegistry(): { [name in string]: OperatorDefinition } {
+  return Object.entries(registry).reduce(
+    (result, [name, item]) => {
+      result[name] = item.definition;
+      return result;
+    },
+    {} as { [name in string]: OperatorDefinition }
+  );
 }
