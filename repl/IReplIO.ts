1cd9932291a8caed04ffe6cf62e112013dc29726
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-10-10 06:40:34 -0400

    refactor: expose IReplInputBuffer

--- repl/IReplIO.ts
+++ repl/IReplIO.ts
@@ -1,11 +1,11 @@
-interface IInputBuffer {
+export interface IReplInputBuffer {
   addLine: (input: string) => void;
 }
 
 export interface IReplIO {
   readonly width: number;
   readonly height: number;
-  setInputBuffer: (buffer: IInputBuffer) => void;
+  setInputBuffer: (buffer: IReplInputBuffer) => void;
   /** Should be a blocking call (no input can be added while waiting for a key) */
   waitForKey: () => Promise<string>;
   output: (text: string) => void;
