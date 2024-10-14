949f2cec92f47d929fef99d68e4a67a4f10e1185
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-10-13 23:50:55 -0400

    feat: common input handling

---
+++ repl/inputHandler.spec.ts
@@ -0,0 +1,64 @@
+import { beforeEach, it, expect } from 'vitest';
+import { buildInputHandler } from './inputHandler.js';
+import type { IInputHandler } from './inputHandler.js';
+
+let inputHandler: IInputHandler;
+let output: string;
+let onData: (data: string) => void;
+
+const simulate = async (text: string | string[]) => {
+  if (typeof text === 'string') {
+    text = text.split('');
+  }
+  for (const data of text) {
+    onData(data);
+    await new Promise((resolve) => setTimeout(resolve, 0));
+  }
+};
+
+beforeEach(() => {
+  output = '';
+  inputHandler = buildInputHandler({
+    width: 40,
+    height: 25,
+    input(callback) {
+      onData = callback;
+    },
+    output(text) {
+      output += text;
+    }
+  });
+});
+
+it('reads a line', async () => {
+  simulate('Hello World !\r');
+  const line = await inputHandler.waitForLines();
+  expect(line).toStrictEqual('Hello World !');
+  expect(output).toStrictEqual('Hello World !\r\n');
+});
+
+it('reads two lines simultaneously', async () => {
+  simulate('Hello World !\rsecond line\r');
+  const lines = await inputHandler.waitForLines();
+  expect(lines).toStrictEqual('Hello World !\nsecond line');
+  expect(output).toStrictEqual('Hello World !\r\nsecond line\r\n');
+});
+
+it('reads two lines sequentially', async () => {
+  const firstKeys = simulate('Hello World !\rsecond');
+  const first = await inputHandler.waitForLines();
+  expect(first).toStrictEqual('Hello World !');
+  expect(output).toStrictEqual('Hello World !\r\nsecond');
+  await firstKeys;
+  simulate(' line\r');
+  const second = await inputHandler.waitForLines();
+  expect(second).toStrictEqual('second line');
+  expect(output).toStrictEqual('Hello World !\r\nsecond line\r\n');
+});
+
+it('reads chars', async () => {
+  simulate('abc');
+  await expect(inputHandler.waitForChar()).resolves.toStrictEqual('a');
+  await expect(inputHandler.waitForChar()).resolves.toStrictEqual('b');
+  await expect(inputHandler.waitForChar()).resolves.toStrictEqual('c');
+});
--- repl/inputHandler.ts
+++ repl/inputHandler.ts
@@ -1,36 +1,102 @@
 import type { IReplIO } from './IReplIO.js';
 
-export function buildInputHandler(replIO: IReplIO): () => Promise<string> {
-  const inputs: string[] = [];
+export interface IInputHandler {
+  waitForLines: () => Promise<string>;
+  waitForChar: () => Promise<string>;
+}
 
-  let newInput: () => void;
-  let waitForInput = Promise.resolve();
-  let newInputTimerId: ReturnType<typeof setTimeout> | undefined;
+export function buildInputHandler(replIO: IReplIO): IInputHandler {
+  const linesBuffer: string[] = [];
 
-  const noInputs = () => {
-    waitForInput = new Promise((resolve) => {
-      newInput = resolve;
+  let linesBufferChange = Promise.resolve();
+
+  let notifyLinesBufferChange = () => {};
+  const appendToLinesBuffer = (data: string, newLine = false) => {
+    if (data.length) {
+      linesBuffer[0] = linesBuffer[0] + data;
+    }
+    if (newLine) {
+      linesBuffer.unshift('');
+    }
+    notifyLinesBufferChange();
+    linesBufferChange = new Promise<void>((resolve) => {
+      notifyLinesBufferChange = resolve;
     });
   };
-  noInputs();
 
-  replIO.setInputBuffer({
-    addLine(input: string) {
-      if (newInputTimerId !== undefined) {
-        clearTimeout(newInputTimerId);
+  appendToLinesBuffer('', true); // Initialize
+
+  replIO.input((data) => {
+    if (data.length > 1) {
+      let unterminatedLine: string;
+      if (data.includes('\r')) {
+        const lines = data.split('\r');
+        unterminatedLine = lines.pop()!; // because it includes \r
+        lines.forEach((line) => {
+          replIO.output(line);
+          replIO.output('\r\n');
+          appendToLinesBuffer(line, true);
+        });
+      } else {
+        unterminatedLine = data;
       }
-      inputs.push(input);
-      newInputTimerId = setTimeout(newInput, 100);
+      if (unterminatedLine.length > 0) {
+        replIO.output(unterminatedLine);
+        appendToLinesBuffer(unterminatedLine);
+      }
+    } else if (data === '\r') {
+      // Enter
+      replIO.output('\r\n');
+      appendToLinesBuffer('', true);
+    } else if (data === '\u007F') {
+      // Backspace (DEL)
+      const line = linesBuffer[0];
+      if (line && line.length > 0) {
+        replIO.output('\b \b');
+        linesBuffer[0] = line.substring(0, line.length - 1);
+      }
+    } else if ((data >= String.fromCharCode(0x20) && data <= String.fromCharCode(0x7e)) || data >= '\u00a0') {
+      replIO.output(data);
+      appendToLinesBuffer(data, false);
     }
   });
 
-  return async () => {
-    if (inputs.length === 0) {
-      await waitForInput;
+  return {
+    async waitForLines() {
+      const waitForLine = async () => {
+        await linesBufferChange;
+        if (linesBuffer.length === 1) {
+          return waitForLine();
+        }
+        // Wait 10 ms before returning lines
+        let newInput = false;
+        await Promise.race([
+          linesBufferChange.then(() => {
+            newInput = true;
+          }),
+          new Promise((resolve) => setTimeout(resolve, 10))
+        ]);
+        if (newInput) {
+          return waitForLine();
+        }
+        return linesBuffer.splice(1).reverse().join('\n');
+      };
+      return waitForLine();
+    },
+
+    async waitForChar() {
+      if (linesBuffer.length === 1 && linesBuffer[0]!.length === 0) {
+        await linesBufferChange;
+      }
+      const line = linesBuffer.at(-1)!;
+      const char = line.charAt(0);
+      const remainder = line.substring(1);
+      if (remainder.length || linesBuffer.length === 1) {
+        linesBuffer[linesBuffer.length - 1] = remainder;
+      } else {
+        linesBuffer.pop();
+      }
+      return char;
     }
-    const input = inputs.join('\n');
-    inputs.length = 0;
-    noInputs();
-    return input;
   };
 }
