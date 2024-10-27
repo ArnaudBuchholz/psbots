7de397bf8930d5c6495f1ecbf7a88540355e48a2
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-10-26 23:38:21 -0400

    refactor: isolate showError

---
+++ repl/showError.ts
@@ -0,0 +1,27 @@
+import type { IReplIO } from './IReplIO.js';
+import { red, white } from './colors.js';
+import { BaseException, InternalException } from '@psbots/engine/sdk';
+
+export function showError(replIO: IReplIO, e: unknown) {
+  if (!(e instanceof BaseException)) {
+    let name: string;
+    let message: string;
+    if (e instanceof Error) {
+      name = e.name;
+      message = e.message;
+    } else {
+      name = 'Unknown error';
+      message = JSON.stringify(e);
+    }
+    if (message.length) {
+      message = ': ' + message;
+    }
+    replIO.output(`${red}💣 ${name}${message}${white}\r\n`);
+  } else {
+    replIO.output(`${red}❌ ${e.message}${white}\r\n`);
+    e.engineStack.forEach((line) => replIO.output(`${red}${line}${white}\r\n`));
+    if (e instanceof InternalException && typeof e.reason === 'object') {
+      replIO.output(`${red}${JSON.stringify(e.reason, undefined, 2)}${white}\r\n`);
+    }
+  }
+}
