e9ab831925929507c5e50b610b3e03cb48a7330a
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-10-29 06:29:40 -0400

    feat: implement debugger

--- repl/debug.ts
+++ repl/debug.ts
@@ -2,7 +2,7 @@ import type { IState } from '@psbots/engine';
 import type { IReplIO } from './IReplIO.js';
 import { toString, TOSTRING_BEGIN_MARKER, TOSTRING_END_MARKER } from '@psbots/engine/sdk';
 import { blue, cyan, green, magenta, red, white, yellow } from './colors.js';
-import { formatMemoryVariation, status } from './status.js';
+import { formatCountVariation, formatMemoryVariation, status } from './status.js';
 import { operands } from './format.js';
 import { formatBytes } from './formatBytes.js';
 
@@ -45,16 +45,67 @@ export async function runWithDebugger({ replIO, state, iterator, waitForChar }: 
       memoryInfo += memoryVariation.formatted;
       memoryInfoLength += memoryVariation.length;
     }
-    let memoryDetails = ` (user: ${formatBytes(state.memoryTracker.byType.user)}, strings: ${formatBytes(state.memoryTracker.byType.string)}, system: ${formatBytes(state.memoryTracker.byType.system)})`
+    const memoryDetails = ` (user: ${formatBytes(state.memoryTracker.byType.user)}, strings: ${formatBytes(state.memoryTracker.byType.string)}, system: ${formatBytes(state.memoryTracker.byType.system)})`;
     if (memoryInfoLength + memoryDetails.length <= spaceLeftForMemory) {
       memoryInfo += `${white}${memoryDetails}`;
       memoryInfoLength += memoryDetails.length;
     }
-    replIO.output(`│${cycleInfo}${magenta}│${memoryInfo}${''.padStart(spaceLeftForMemory - memoryInfoLength, ' ')}${magenta}│`);
-    replIO.output(`${magenta}├${''.padStart(cycleInfoLength, '─')}┴${''.padStart(spaceLeftForMemory, '─')}┤`);
+    replIO.output(
+      `│${cycleInfo}${magenta}│${memoryInfo}${''.padStart(spaceLeftForMemory - memoryInfoLength, ' ')}${magenta}│`
+    );
 
     const operandsWidth = Math.floor((width - 3) / 2);
     const callStackWidth = width - 3 - operandsWidth;
+    replIO.output(
+      `${magenta}├${''.padStart(cycleInfoLength, '─')}┴${''.padStart(operandsWidth - cycleInfoLength - 1, '─')}┬${''.padStart(callStackWidth, '─')}┤`
+    );
+
+    const operandsVariation = formatCountVariation(lastOperandsCount, state.operands.length);
+    let operandsInfo = `${yellow}O${white}perands: ${yellow}${state.operands.length}`;
+    let operandsInfoSize = 10 + state.operands.length.toString().length;
+    if (operandsInfoSize + operandsVariation.length < operandsWidth) {
+      operandsInfo += operandsVariation.formatted;
+      operandsInfoSize += operandsVariation.length;
+    }
+    replIO.output(`${magenta}│${operandsInfo}${''.padStart(operandsWidth - operandsInfoSize, ' ')}`);
+
+    const callStackVariation = formatCountVariation(lastCallStackSize, state.callStack.length);
+    let callStackInfo = `${white}C${yellow}a${white}ll stack: ${yellow}${state.callStack.length}`;
+    let callStackInfoSize = 12 + state.callStack.length.toString().length;
+    if (callStackInfoSize + callStackVariation.length < callStackWidth) {
+      callStackInfo += callStackVariation.formatted;
+      callStackInfoSize += callStackVariation.length;
+    }
+    replIO.output(`${magenta}│${callStackInfo}${''.padStart(callStackWidth - callStackInfoSize, ' ')}${magenta}│`);
+
+    replIO.output(`${magenta}│${''.padStart(operandsWidth, ' ')}│${''.padStart(callStackWidth, ' ')}│`);
+
+    const callStack = state.callStack;
+    for (let index = 0; index < 5; ++index) {
+      replIO.output(`${magenta}│${white}`);
+
+      const operand = state.operands.at(index);
+      if (null === operand) {
+        replIO.output(`${magenta}│${''.padStart(operandsWidth, ' ')}`);
+      } else {
+        const operandInfo = toString(operand, { maxWidth: operandsWidth });
+        replIO.output(`${operandInfo}${''.padStart(operandsWidth - operandInfo.length, ' ')}`);
+      }
+
+      replIO.output(`${magenta}│${white}`);
+
+      if (index < callStack.length) {
+        const { value, operatorState } = state.callStack[index]!;
+        const callStackInfo = toString(value, { maxWidth: callStackWidth, operatorState });
+        const callStackInfoSize = callStackInfo.length;
+        replIO.output(`${callStackInfo}${''.padStart(callStackWidth - callStackInfoSize, ' ')}`);
+      } else {
+        replIO.output(`${''.padStart(callStackWidth, ' ')}`);
+      }
+      replIO.output(`${magenta}│`);
+    }
+
+    replIO.output(`\n`);
     replIO.output(`${magenta}│${''.padStart(operandsWidth, ' ')}│${''.padStart(callStackWidth, ' ')}│`);
 
     replIO.output('\n');
