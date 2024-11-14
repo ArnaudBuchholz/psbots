9937bdae66378e3a176606adc821d481c1126f52
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-11-14 07:02:19 -0500

    refactor: use testIsFunction

--- engine/src/sdk/checks/isObject.spec.ts
+++ engine/src/sdk/checks/isObject.spec.ts
@@ -1,12 +1,8 @@
-import { testCheckFunction, values } from '@test/index.js';
+import { testIsFunction, values } from '@test/index.js';
 import { isObject } from '@sdk/checks/isObject.js';
 
-testCheckFunction<object>({
-  check: (value) => {
-    if (!isObject(value)) {
-      throw new Error();
-    }
-  },
+testIsFunction<object>({
+  is: isObject,
   valid: [{}, /test/, []],
   invalid: [null, ...values.numbers, ...values.functions, '', 'Hello World !']
 });
