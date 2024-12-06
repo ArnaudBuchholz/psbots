5297a4899142df9b44771b46da7708d3fa9678cb
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-12-06 07:58:49 -0500

    refactor: adjust coding to new constraints

--- engine/src/core/state/call.ts
+++ engine/src/core/state/call.ts
@@ -2,15 +2,18 @@ import type { Value, ValueType } from '@api/index.js';
 import { OPERATOR_STATE_FIRST_CALL } from '@sdk/index.js';
 import type { IInternalState } from '@sdk/index.js';
 
-export function callCycle(
-  { dictionaries, calls, callEnabled, operands }: IInternalState,
-  value: Value<ValueType.name>
-): void {
+export function callCycle(state: IInternalState, value: Value<ValueType.name>): void {
+  const { dictionaries, calls, callEnabled, operands } = state;
   if (calls.topOperatorState === OPERATOR_STATE_FIRST_CALL) {
     calls.pop();
   } else if (callEnabled || ['{', '}', '<<', '«', '>>', '»'].includes(value.name)) {
     calls.topOperatorState = OPERATOR_STATE_FIRST_CALL;
-    const entry = dictionaries.lookup(value.name);
+    const result = dictionaries.lookup(value.name);
+    if (!result.success) {
+      state.exception = result.error;
+      return;
+    }
+    const entry = result.value;
     if (value.debugSource) {
       calls.push(
         Object.assign(
