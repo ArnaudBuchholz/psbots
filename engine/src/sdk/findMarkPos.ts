3d5b5004bd0667b0698c8b8b0f22cefa4492845f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-12-17 08:04:00 -0500

    refactor: migrate helpers to Result

--- engine/src/sdk/findMarkPos.ts
+++ engine/src/sdk/findMarkPos.ts
@@ -1,11 +1,11 @@
-import { ValueType } from '@api/index.js';
+import { Result, ValueType } from '@api/index.js';
 import type { IStack } from '@sdk/interfaces/IStack.js';
 import { UnmatchedMarkException } from '@sdk/exceptions/UnmatchedMarkException.js';
 
-export function findMarkPos(stack: IStack): number {
+export function findMarkPos(stack: IStack): Result<number> {
   const pos = stack.ref.findIndex((value) => value.type === ValueType.mark);
   if (pos === -1) {
-    throw new UnmatchedMarkException();
+    return { success: false, error: new UnmatchedMarkException() };
   }
-  return pos;
+  return { success: true, value: pos };
 }
--- engine/src/sdk/toValue.ts
+++ engine/src/sdk/toValue.ts
@@ -1,4 +1,4 @@
-import type { IValueTracker, Value } from '@api/index.js';
+import type { IValueTracker, Result, Value } from '@api/index.js';
 import { ValueType } from '@api/index.js';
 import { UndefinedResultException } from '@sdk/exceptions/UndefinedResultException.js';
 
@@ -11,15 +11,18 @@ export function toBooleanValue(isSet: boolean): Value<ValueType.boolean> {
   };
 }
 
-export function toIntegerValue(integer: number): Value<ValueType.integer> {
+export function toIntegerValue(integer: number): Result<Value<ValueType.integer>> {
   if (integer % 1 !== 0 || integer < Number.MIN_SAFE_INTEGER || integer > Number.MAX_SAFE_INTEGER) {
-    throw new UndefinedResultException();
+    return { success: false, error: new UndefinedResultException() };
   }
   return {
-    type: ValueType.integer,
-    isExecutable: false,
-    isReadOnly: true,
-    integer
+    success: true,
+    value: {
+      type: ValueType.integer,
+      isExecutable: false,
+      isReadOnly: true,
+      integer
+    }
   };
 }
 
