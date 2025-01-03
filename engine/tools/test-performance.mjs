0d7fb098bae631123089b60fc21525f5f7aca208
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-01-03 16:32:15 -0500

    feat: display performances

--- engine/tools/test-performance.mjs
+++ engine/tools/test-performance.mjs
@@ -1,21 +1,23 @@
 import { hrtime } from 'node:process';
+import { writeFile } from 'node:fs/promises';
 import { createState, getOperatorDefinitionRegistry, ValueType } from '../dist/index.js';
 import { toStringValue } from '../dist/sdk/index.js';
 
-const red = '\x1b[31m';
-const green = '\x1b[32m';
+// const red = '\x1b[31m';
+// const green = '\x1b[32m';
 const yellow = '\x1b[33m';
-const blue = '\x1b[34m';
+// const blue = '\x1b[34m';
 const magenta = '\x1b[35m';
-const cyan = '\x1b[36m';
+// const cyan = '\x1b[36m';
 const white = '\x1b[37m';
 
 const LOOPS = 500;
+const MAX_CYCLES = 10 ** 9;
 
 let sampleCount = 0;
 let cycles = 0;
 let timeSpent = 0; // nanoseconds
-const operators = {};
+const measurements = {};
 
 function execute(source) {
   const { value: state, error } = createState();
@@ -23,20 +25,20 @@ function execute(source) {
     throw error;
   }
   const iterator = state.exec(toStringValue(source, { isExecutable: true }));
-  while (true) {
-    ++cycles;
+  while (++cycles < MAX_CYCLES) {
     const { callStack } = state;
-    let operator;
+    let instruction;
+
     if (callStack[0]?.value.type === ValueType.operator) {
-      operator = callStack[0].value.operator.name;
+      instruction = callStack[0].value.operator.name;
     }
     const start = hrtime();
     const { done } = iterator.next();
     const duration = hrtime(start)[1];
     timeSpent += duration;
-    if (operator) {
-      operators[operator] ??= [];
-      operators[operator].push(duration);
+    if (instruction) {
+      measurements[instruction] ??= [];
+      measurements[instruction].push(duration);
     }
     if (done === true) {
       break;
@@ -64,17 +66,42 @@ console.log(`Version   :${magenta}`, version, white);
 console.log('Samples   :', sampleCount);
 console.log('Cycles    :', cycles);
 console.log(`Duration  :${yellow}`, timeSpent.toLocaleString(), `${white}ns`);
-console.log('Cycles/ms :', Math.floor((10 ** 9) * cycles / timeSpent) / 1000);
+console.log('Cycles/ms :', Math.floor((10 ** 9 * cycles) / timeSpent) / 1000);
 
-const operatorNames = Object.keys(operators).sort();
-const operatorStatistics = {};
-for(const operator of operatorNames) {
-  const durations = operators[operator];
+const intructions = Object.keys(measurements).sort();
+const statistics = {};
+for (const instruction of intructions) {
+  const durations = measurements[instruction];
   const totalDuration = durations.reduce((total, duration) => duration + total);
   const mean = Math.floor(totalDuration / durations.length);
-  operatorStatistics[operator] = {
+  const variance = Math.floor(
+    Math.sqrt(durations.reduce((total, duration) => total + (mean - duration) ** 2, 0) / (durations.length - 1))
+  );
+  const halfPercentile = Math.floor(
+    (100 * durations.reduce((total, duration) => total + (2 * duration <= mean ? 1 : 0), 0)) / durations.length
+  );
+  const meanPercentile = Math.floor(
+    (100 * durations.reduce((total, duration) => total + (duration <= mean ? 1 : 0), 0)) / durations.length
+  );
+  const twicePercentile = Math.floor(
+    (100 * durations.reduce((total, duration) => total + (duration >= 2 * mean ? 1 : 0), 0)) / durations.length
+  );
+  statistics[instruction] = {
     count: durations.length,
-    'mean (ns)': mean.toLocaleString().padStart(6, ' ')
-  }
+    't (ns)': mean,
+    Δ: variance,
+    '≤t%': meanPercentile,
+    '≤½t%': halfPercentile,
+    '≥2t%': twicePercentile
+  };
+  measurements[instruction] = {
+    durations,
+    mean,
+    variance,
+    meanPercentile,
+    twicePercentile
+  };
 }
-console.table(operatorStatistics);
+console.table(statistics);
+
+await writeFile('./performances.txt', JSON.stringify(measurements, undefined, 2));
