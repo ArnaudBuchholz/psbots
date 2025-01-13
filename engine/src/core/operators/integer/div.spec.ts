9b86128e4919139d790335fe747f5316dd7d100c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-01-13 00:01:05 -0500

    test: increase coverage

---
+++ engine/src/core/operators/integer/div.spec.ts
@@ -0,0 +1,41 @@
+import { it, expect, vi } from 'vitest';
+import { Exception, ValueType } from '@api/index.js';
+import { assert, toIntegerValue } from '@sdk/index.js'
+import { State } from '@core/state/State.js';
+import { toValue, waitForExec } from '@test/index.js';
+
+vi.mock('@sdk/index.js', async () => {
+  const actual = await vi.importActual('@sdk/index.js');
+  return {
+    ...actual,
+    toIntegerValue: vi.fn()
+  };
+});
+
+it('fails if the resulting reminder is not valid', async () => {
+  const stateResult = State.create();
+  assert(stateResult);
+  const { value: state } = stateResult;
+  vi.mocked(toIntegerValue).mockImplementation((value) => {
+    if (value === 0) {
+      return { success: false, exception: 'limitcheck' };
+    }
+    return { success: true, value: { type: ValueType.integer, isExecutable: false, isReadOnly: true, integer: 1 } };
+  });
+  await waitForExec(state.exec(toValue('1 1 div', { isExecutable: true })));
+  expect(state.exception).toStrictEqual<Exception>('limitcheck');
+});
+
+it('fails if the resulting dividend is not valid', async () => {
+  const stateResult = State.create();
+  assert(stateResult);
+  const { value: state } = stateResult;
+  vi.mocked(toIntegerValue).mockImplementation((value) => {
+    if (value === 1) {
+      return { success: false, exception: 'limitcheck' };
+    }
+    return { success: true, value: { type: ValueType.integer, isExecutable: false, isReadOnly: true, integer: 0 } };
+  });
+  await waitForExec(state.exec(toValue('1 1 div', { isExecutable: true })));
+  expect(state.exception).toStrictEqual<Exception>('limitcheck');
+});
