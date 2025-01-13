417cac1f6c5217d6fb95b49cbcda2f45fcda5fca
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-01-13 00:11:02 -0500

    test: increase coverage

---
+++ engine/src/core/operators/stacks/def.spec.ts
@@ -0,0 +1,17 @@
+import { it, expect, vi } from 'vitest';
+import { Exception } from '@api/index.js';
+import { assert } from '@sdk/index.js';
+import { State } from '@core/state/State.js';
+import { Dictionary } from '@core/objects/dictionaries/Dictionary.js';
+import { toValue, waitForExec } from '@test/index.js';
+
+it('forwards Dictionary::def error', async () => {
+  const stateResult = State.create();
+  assert(stateResult);
+  const { value: state } = stateResult;
+  const dictDef = vi.spyOn(Dictionary.prototype, 'def');
+  dictDef.mockImplementation(() => ({ success: false, exception: 'limitcheck' }));
+  await waitForExec(state.exec(toValue('/test 1 def', { isExecutable: true })));
+  dictDef.mockRestore();
+  expect(state.exception).toStrictEqual<Exception>('limitcheck');
+});
