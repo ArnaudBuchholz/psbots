7d82b888fd0d3b4af25f82b5f4f3e7b306ab91d2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-01-13 00:01:09 -0500

    test: increase coverage

---
+++ engine/src/core/operators/integer/mul.spec.ts
@@ -0,0 +1,23 @@
+import { it, expect, vi } from 'vitest';
+import { Exception } from '@api/index.js';
+import { assert, toIntegerValue } from '@sdk/index.js'
+import { State } from '@core/state/State.js';
+import { toValue, waitForExec } from '@test/index.js';
+
+vi.mock('@sdk/index.js', async () => {
+  const actual = await vi.importActual('@sdk/index.js');
+  return {
+    ...actual,
+    toIntegerValue: vi.fn()
+  };
+});
+
+
+it('fails if the resulting integer is not valid', async () => {
+  const stateResult = State.create();
+  assert(stateResult);
+  const { value: state } = stateResult;
+  vi.mocked(toIntegerValue).mockImplementation(() => ({ success: false, exception: 'limitcheck' }));
+  await waitForExec(state.exec(toValue('1 2 mul', { isExecutable: true })));
+  expect(state.exception).toStrictEqual<Exception>('limitcheck');
+});
