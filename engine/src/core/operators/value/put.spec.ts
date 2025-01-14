1cf9ce3f1bda06ef19f4ec17914a8b1cea3ef656
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-01-14 05:58:02 -0500

    test: increase coverage

---
+++ engine/src/core/operators/value/put.spec.ts
@@ -0,0 +1,53 @@
+import { it, expect, vi } from 'vitest';
+import { ValueType, type Exception } from '@api/index.js';
+import { assert } from '@sdk/index.js';
+import { State } from '@core/state/State.js';
+import { toValue, waitForExec } from '@test/index.js';
+import { MemoryTracker } from '@core/MemoryTracker.js';
+import { Dictionary } from '@core/objects/dictionaries/Dictionary.js';
+import { ValueArray } from '@core/objects/ValueArray.js';
+
+it('fails when no more memory is available', async () => {
+  const stateResult = State.create();
+  assert(stateResult);
+  const { value: state } = stateResult;
+  const originalAddStringRef = MemoryTracker.prototype.addStringRef;
+  const addStringRef = vi.spyOn(MemoryTracker.prototype, 'addStringRef');
+  addStringRef.mockImplementation(function (this: MemoryTracker, string) {
+    if (string === 'a23') {
+      return { success: false, exception: 'vmOverflow' };
+    }
+    return originalAddStringRef.call(this, string);
+  });
+  await waitForExec(state.exec(toValue('"123" 0 97 put', { isExecutable: true })));
+  addStringRef.mockRestore();
+  expect(state.exception).toStrictEqual<Exception>('vmOverflow');
+});
+
+it('forwards Array::set error', async () => {
+  const stateResult = State.create();
+  assert(stateResult);
+  const { value: state } = stateResult;
+  const originaArraySet = ValueArray.prototype.set;
+  const arraySet = vi.spyOn(ValueArray.prototype, 'set');
+  arraySet.mockImplementation(function (this: ValueArray, index, value) {
+    if (index === 1 && value.type === ValueType.string) {
+      return { success: false, exception: 'limitcheck' };
+    }
+    return originaArraySet.call(this, index, value);
+  });
+  await waitForExec(state.exec(toValue('[ 1 2 3 ] 1 "a" put', { isExecutable: true })));
+  arraySet.mockRestore();
+  expect(state.exception).toStrictEqual<Exception>('limitcheck');
+});
+
+it('forwards Dictionary::def error', async () => {
+  const stateResult = State.create();
+  assert(stateResult);
+  const { value: state } = stateResult;
+  const dictDef = vi.spyOn(Dictionary.prototype, 'def');
+  dictDef.mockImplementation(() => ({ success: false, exception: 'limitcheck' }));
+  await waitForExec(state.exec(toValue('userdict /test 123 put', { isExecutable: true })));
+  dictDef.mockRestore();
+  expect(state.exception).toStrictEqual<Exception>('limitcheck');
+});
