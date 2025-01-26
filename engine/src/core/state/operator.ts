a1662a0a98dc50d68cdf7a29d85e543890fa1a81
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-01-26 00:28:37 -0500

    documentation: missing test

--- engine/src/core/state/operator.ts
+++ engine/src/core/state/operator.ts
@@ -14,6 +14,7 @@ import { MemoryTracker } from '@core/MemoryTracker.js';
 export function operatorPop(state: IInternalState, value: Value<ValueType.operator>): void {
   const { calls } = state;
   const operator = value.operator as IFunctionOperator;
+  // TODO: test calls.topOperatorState >= OPERATOR_STATE_FIRST_CALL
   if (calls.topOperatorState >= OPERATOR_STATE_FIRST_CALL || calls.topOperatorState === OPERATOR_STATE_POP) {
     calls.pop();
   } else {
