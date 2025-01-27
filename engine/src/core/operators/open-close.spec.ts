d7c887d07ec143b107c72181527709e527c4fafe
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-01-26 23:13:46 -0500

    refactor: improve test

--- engine/src/core/operators/open-close.spec.ts
+++ engine/src/core/operators/open-close.spec.ts
@@ -1,8 +1,9 @@
 import { it, expect, beforeEach, afterEach, vi, describe } from 'vitest';
 import type { Exception, IDebugSource } from '@api/index.js';
+import { markValue } from '@api/index.js';
 import { assert, OPERATOR_STATE_FIRST_CALL } from '@sdk/index.js';
-import { State } from '@core/state/State.js';
 import { toValue, waitForExec } from '@test/index.js';
+import { State } from '@core/state/State.js';
 import { CallStack } from '@core/objects/stacks/CallStack.js';
 import { ValueArray } from '@core/objects/ValueArray.js';
 import { ValueStack } from '@core/objects/stacks/ValueStack.js';
@@ -58,6 +59,8 @@ describe('error handling', () => {
       done = run.next().done;
     }
     expect(maxIterations).toBeGreaterThan(0);
+    expect(state.operands.at(0)).toStrictEqual(toValue(1));
+    expect(state.operands.at(1)).toStrictEqual(markValue);
   });
 
   it('fails if not able to store mark position in stack', () => {
@@ -94,37 +97,40 @@ describe('error handling', () => {
       expect(state.calls.operatorStateAt(0)).toStrictEqual(OPERATOR_STATE_ALLOC_ARRAY);
       def.mockRestore();
     });
+  });
 
-    describe('setting array items', () => {
-      beforeEach(() => {
-        run.next();
-      });
+  describe('setting array items', () => {
+    beforeEach(() => {
+      run.next();
+      run.next();
+    });
 
-      it('fails if not able to store an item in the array', () => {
-        const set = vi.spyOn(ValueArray.prototype, 'set');
-        set.mockImplementation(() => ({ success: false, exception: 'limitcheck' }));
-        run.next();
-        expect(state.exception).toStrictEqual<Exception>('limitcheck');
-        // Reset operator state
-        expect(state.calls.operatorStateAt(0)).toStrictEqual(OPERATOR_STATE_ALLOC_ARRAY);
-        set.mockRestore();
-      });
+    it('fails if not able to store an item in the array', () => {
+      const set = vi.spyOn(ValueArray.prototype, 'set');
+      set.mockImplementation(() => ({ success: false, exception: 'limitcheck' }));
+      run.next();
+      expect(state.exception).toStrictEqual<Exception>('limitcheck');
+      // Reset operator state
+      expect(state.calls.operatorStateAt(0)).toStrictEqual(OPERATOR_STATE_ALLOC_ARRAY);
+      set.mockRestore();
+    });
+  });
 
-      describe('final step', () => {
-        beforeEach(() => {
-          run.next();
-        });
+  describe('final step', () => {
+    beforeEach(() => {
+      run.next();
+      run.next();
+      run.next();
+    });
 
-        it('fails if not able to add the array in the operands stack', () => {
-          const popush = vi.spyOn(ValueStack.prototype, 'popush');
-          popush.mockImplementation(() => ({ success: false, exception: 'limitcheck' }));
-          run.next();
-          expect(state.exception).toStrictEqual<Exception>('limitcheck');
-          // Reset operator state
-          expect(state.calls.operatorStateAt(0)).toStrictEqual(OPERATOR_STATE_ALLOC_ARRAY);
-          popush.mockRestore();
-        });
-      });
+    it('fails if not able to add the array in the operands stack', () => {
+      const popush = vi.spyOn(ValueStack.prototype, 'popush');
+      popush.mockImplementation(() => ({ success: false, exception: 'limitcheck' }));
+      run.next();
+      expect(state.exception).toStrictEqual<Exception>('limitcheck');
+      // Reset operator state
+      expect(state.calls.operatorStateAt(0)).toStrictEqual(OPERATOR_STATE_ALLOC_ARRAY);
+      popush.mockRestore();
     });
   });
 });
