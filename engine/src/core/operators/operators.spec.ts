f0f9b3833b3119f127217c73d2586683f3d81b93
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-02-01 23:18:40 -0500

    refactor: remove performance part (now isolated)

--- engine/src/core/operators/operators.spec.ts
+++ engine/src/core/operators/operators.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach, afterEach, onTestFailed } from 'vitest';
+import { describe, it, expect, beforeEach, afterEach } from 'vitest';
 import { enumIArrayValues, parse } from '@api/index.js';
 import type { OperatorDefinition } from './operators.js';
 import { registry } from './operators.js';
@@ -12,11 +12,6 @@ const nullDefinition: OperatorDefinition = {
   labels: [],
   samples: []
 };
-
-let skipped = 0;
-let debugCycles = 0;
-let debugMilliseconds = 0;
-let failed = false;
 
 describe('executing in & out using debug', () => {
   Object.keys(registry)
@@ -55,16 +50,10 @@ describe('executing in & out using debug', () => {
           const description = sample.description ?? definition.description;
           if (missingOperators.length > 0) {
             it.skip(`[${sampleId}] ${description} (⚠️ ${missingOperators})`);
-            ++skipped;
           } else {
             it(`[${sampleId}] ${description}`, () => {
-              onTestFailed(() => {
-                failed = true;
-              });
-              const start = performance.now();
-              debugCycles += waitForExec(state.exec(toValue(sample.in, { isExecutable: true }))).length;
-              debugCycles += waitForExec(expectedState.exec(toValue(sample.out, { isExecutable: true }))).length;
-              debugMilliseconds += Math.ceil(performance.now() - start);
+              waitForExec(state.exec(toValue(sample.in, { isExecutable: true })));
+              waitForExec(expectedState.exec(toValue(sample.out, { isExecutable: true })));
               if (expectedState.exception) {
                 expect(state.exception).not.toBeUndefined();
                 expect(state.exception).toStrictEqual(expectedState.exception);
@@ -84,87 +73,4 @@ describe('executing in & out using debug', () => {
         });
       });
     });
-});
-
-let cycles = 0;
-let milliseconds = 0;
-let minMs = Number.POSITIVE_INFINITY;
-let maxMs = 0;
-
-describe.runIf(!failed)('executing in only (performance)', () => {
-  Object.keys(registry)
-    .sort()
-    .forEach((operatorName) => {
-      const definition: OperatorDefinition = registry[operatorName]?.definition ?? nullDefinition;
-      if (!definition.samples.length) {
-        return;
-      }
-      describe(`operators/${operatorName}`, () => {
-        let state: State;
-
-        beforeEach(() => {
-          const stateResult = State.create();
-          assert(stateResult);
-          state = stateResult.value;
-        });
-
-        afterEach(() => {
-          state.destroy();
-        });
-
-        definition.samples.forEach((sample, index) => {
-          const missingOperators = [...parse(sample.in), ...parse(sample.out)]
-            .filter((value) => value.type === 'string' && value.isExecutable)
-            .map((value) => (value.type === 'string' ? value.string : ''))
-            .filter((name) => !Object.prototype.hasOwnProperty.call(registry, name) && !name.startsWith('test'))
-            .reduce((names, name) => (names.includes(name) ? names : [name, ...names]), <string[]>[]);
-          const sampleId = `${operatorName}#${index}`;
-          const description = sample.description ?? definition.description;
-          if (missingOperators.length === 0) {
-            it(`[${sampleId}] ${description}`, () => {
-              const execResult = state.exec(toValue(sample.in, { isExecutable: true }));
-              assert(execResult);
-              const iterator = execResult.value;
-              // eslint-disable-next-line no-constant-condition
-              while (true) {
-                const start = performance.now();
-                const { done } = iterator.next();
-                const timeSpent = performance.now() - start;
-                ++cycles;
-                milliseconds += timeSpent;
-                minMs = Math.min(minMs, timeSpent);
-                maxMs = Math.max(maxMs, timeSpent);
-                if (done === true) {
-                  break;
-                }
-              }
-            });
-          }
-        });
-      });
-    });
-});
-
-describe('Execution report for operators', () => {
-  if (skipped === 1) {
-    it.skip('1 test is skipped');
-  } else if (skipped > 0) {
-    it.skip(`${skipped} tests are skipped`);
-  }
-
-  const round = (value: number) => Math.floor(value * 10000) / 10000;
-
-  it('(debug) cycles/ms > 0', () => {
-    const debugRatio = round(debugCycles / debugMilliseconds);
-    console.log('(debug) cycles/ms', debugRatio);
-    expect(debugRatio).toBeGreaterThan(0);
-  });
-
-  it.runIf(!failed)('cycles/ms > (debug) cycles/ms', () => {
-    const ratio = round(cycles / milliseconds);
-    console.log('cycles/ms', ratio);
-    console.log('cycle (ms)', round(minMs), '<', round(milliseconds / cycles), '<', round(maxMs));
-    const debugRatio = round(debugCycles / debugMilliseconds);
-    expect(ratio).toBeGreaterThan(debugRatio);
-  });
 });
