591155456d5c6f23666a1fead5cb748a37e23aaa
Arnaud Buchholz <arnaud.buchholz@free.fr>
2025-03-28 07:53:32 -0400

    refactor: fixed linting issue

--- repl/debug.ts
+++ repl/debug.ts
@@ -17,27 +17,158 @@ const border = magenta;
 const shortcut = green;
 
 function colorize(string: string): string {
-  // const chars = [...string];
-  // const beginMarkerPos = chars.indexOf(TOSTRING_BEGIN_MARKER);
-  // const endMarkerPos = chars.indexOf(TOSTRING_END_MARKER, beginMarkerPos);
-  // if (beginMarkerPos > -1 && endMarkerPos > -1) {
-  //   chars.splice(endMarkerPos + 1, 0, white);
-  //   chars.splice(beginMarkerPos + 1, 0, yellow);
-  // }
-  // const sourcePos = chars.lastIndexOf('@');
-  // if (sourcePos > -1) {
-  //   chars.splice(sourcePos + 1, 0, blue);
-  //   chars.push(white);
-  // }
-  // TODO: 
-  return string
-    .replaceAll(
-      // eslint-disable-next-line security/detect-non-literal-regexp -- both are constants
-      new RegExp(TOSTRING_BEGIN_MARKER + '.*' + TOSTRING_END_MARKER, 'g'),
-      (match: string): string => `${yellow}${match}${white}`
-    )
-    .replaceAll(/@.+$/g, (match: string): string => `${blue}${match}${white}`)
-    .replaceAll(/[…↵⭲]/g, (match: string): string => `${blue}${match}${white}`);
+  const chars = [...string];
+  const beginMarkerPos = chars.indexOf(TOSTRING_BEGIN_MARKER);
+  const endMarkerPos = chars.indexOf(TOSTRING_END_MARKER, beginMarkerPos);
+  if (beginMarkerPos !== -1 && endMarkerPos !== -1) {
+    chars.splice(endMarkerPos + 1, 0, white);
+    chars.splice(beginMarkerPos, 0, yellow);
+  }
+  const sourcePos = chars.lastIndexOf('@');
+  if (sourcePos !== -1) {
+    chars.splice(sourcePos, 0, blue);
+    chars.push(white);
+  }
+  return chars.map((char) => ('…↵⭲'.includes(char) ? `${blue}${char}${white}` : char)).join('');
+}
+
+function renderCycleAndMemoryInfo({
+  replIO,
+  state,
+  cycle,
+  width,
+  lastUsedMemory
+}: {
+  replIO: IReplIO;
+  state: IState;
+  cycle: number;
+  width: number;
+  lastUsedMemory: number;
+}): number {
+  const cycleLength = cycle.toString().length;
+  const cycleInfoLength = 8 + cycleLength;
+  const cycleInfo = `${white}Cycle: #${yellow}${cycle.toString()}`;
+
+  const spaceLeftForMemory = width - 3 /* ┌┬┐ */ - cycleInfoLength;
+
+  replIO.output(`${border}┌${''.padStart(cycleInfoLength, '─')}┬${''.padStart(spaceLeftForMemory, '─')}┐`);
+
+  const memoryVariation = formatMemoryVariation(lastUsedMemory, state.memoryTracker.used);
+  const currentMemory = formatBytes(state.memoryTracker.used);
+  let memoryInfoLength = 8 + currentMemory.length;
+  let memoryInfo = `${shortcut}M${white}emory: ${yellow}${currentMemory}`;
+  if (memoryInfoLength + memoryVariation.length < spaceLeftForMemory) {
+    memoryInfo += memoryVariation.formatted;
+    memoryInfoLength += memoryVariation.length;
+  }
+  const memoryDetails = ` (user: ${formatBytes(state.memoryTracker.byType.user)}, strings: ${formatBytes(state.memoryTracker.byType.string)}, system: ${formatBytes(state.memoryTracker.byType.system)})`;
+  if (memoryInfoLength + memoryDetails.length <= spaceLeftForMemory) {
+    memoryInfo += `${white}${memoryDetails}`;
+    memoryInfoLength += memoryDetails.length;
+  }
+  replIO.output(
+    `│${cycleInfo}${border}│${memoryInfo}${''.padStart(spaceLeftForMemory - memoryInfoLength, ' ')}${border}│`
+  );
+
+  return cycleInfoLength;
+}
+
+function renderOperandAndCallStacksTitle({
+  replIO,
+  state,
+  width,
+  cycleInfoLength,
+  lastOperandsCount,
+  lastCallStackSize
+}: {
+  replIO: IReplIO;
+  state: IState;
+  width: number;
+  cycleInfoLength: number;
+  lastOperandsCount: number;
+  lastCallStackSize: number;
+}): {
+  operandsWidth: number;
+  callStackWidth: number;
+} {
+  const operandsWidth = Math.floor((width - 3) / 2);
+  const callStackWidth = width - 3 - operandsWidth;
+  replIO.output(
+    `${border}├${''.padStart(cycleInfoLength, '─')}┴${''.padStart(operandsWidth - cycleInfoLength - 1, '─')}┬${''.padStart(callStackWidth, '─')}┤`
+  );
+
+  const operandsVariation = formatCountVariation(lastOperandsCount, state.operands.length);
+  let operandsInfo = `${shortcut}O${white}perands: ${yellow}${state.operands.length}`;
+  let operandsInfoSize = 10 + state.operands.length.toString().length;
+  if (operandsInfoSize + operandsVariation.length < operandsWidth) {
+    operandsInfo += operandsVariation.formatted;
+    operandsInfoSize += operandsVariation.length;
+  }
+  replIO.output(`${border}│${operandsInfo}${''.padStart(operandsWidth - operandsInfoSize, ' ')}`);
+
+  const callStackVariation = formatCountVariation(lastCallStackSize, state.calls.length);
+  let callStackInfo = `${white}C${shortcut}a${white}ll stack: ${yellow}${state.calls.length}`;
+  let callStackInfoSize = 12 + state.calls.length.toString().length;
+  let exceptionInfo = '';
+  let exceptionInfoSize = 0;
+  if (state.exception) {
+    exceptionInfo = ` ${red}❌${state.exception}`;
+    exceptionInfoSize = state.exception.length + 2;
+  }
+  if (callStackInfoSize + callStackVariation.length + exceptionInfoSize < callStackWidth) {
+    callStackInfo += callStackVariation.formatted + exceptionInfo;
+    callStackInfoSize += callStackVariation.length + exceptionInfoSize;
+  } else if (callStackInfoSize + exceptionInfoSize < callStackWidth) {
+    callStackInfo += exceptionInfo;
+    callStackInfoSize += exceptionInfoSize;
+  } else if (state.exception && callStackInfoSize + 2 < callStackWidth) {
+    callStackInfo += ' ❌';
+    callStackInfoSize += 2;
+  }
+
+  replIO.output(`${border}│${callStackInfo}${''.padStart(callStackWidth - callStackInfoSize, ' ')}${border}│`);
+
+  return { operandsWidth, callStackWidth };
+}
+
+function renderOperandAndCallStacks({
+  replIO,
+  state,
+  operandsWidth,
+  callStackWidth
+}: {
+  replIO: IReplIO;
+  state: IState;
+  operandsWidth: number;
+  callStackWidth: number;
+}): void {
+  const callStack = state.calls;
+  for (let index = 0; index < 5; ++index) {
+    replIO.output(`${border}│${white}`);
+
+    const operand = state.operands.at(index);
+    if (null === operand) {
+      replIO.output(''.padStart(operandsWidth, ' '));
+    } else {
+      const operandInfo = toString(operand, { maxWidth: operandsWidth, includeDebugSource: true });
+      replIO.output(`${colorize(operandInfo)}${''.padStart(operandsWidth - operandInfo.length, ' ')}`);
+    }
+
+    replIO.output(`${border}│${white}`);
+
+    if (index < callStack.length) {
+      const value = state.calls.at(index);
+      const operatorState = state.calls.operatorStateAt(index);
+      const callStackInfo = toString(value, { maxWidth: callStackWidth, operatorState, includeDebugSource: true });
+      const callStackInfoSize = callStackInfo.length;
+      replIO.output(`${colorize(callStackInfo)}${''.padStart(callStackWidth - callStackInfoSize, ' ')}`);
+    } else {
+      replIO.output(''.padStart(callStackWidth, ' '));
+    }
+    replIO.output(`${border}│`);
+  }
+
+  replIO.output(`${border}└${''.padStart(operandsWidth, '─')}┴${''.padStart(callStackWidth, '─')}┘`);
 }
 
 export async function runWithDebugger({ replIO, state, iterator, waitForChar }: DebugParameters): Promise<number> {
@@ -56,96 +187,16 @@ export async function runWithDebugger({ replIO, state, iterator, waitForChar }: 
     }
 
     replIO.output('\u001B[1;1H\u001B[J'); // clear display
-
-    const cycleLength = cycle.toString().length;
-    const cycleInfoLength = 8 + cycleLength;
-    const cycleInfo = `${white}Cycle: #${yellow}${cycle.toString()}`;
-
-    const spaceLeftForMemory = width - 3 /* ┌┬┐ */ - cycleInfoLength;
-
-    replIO.output(`${border}┌${''.padStart(cycleInfoLength, '─')}┬${''.padStart(spaceLeftForMemory, '─')}┐`);
-
-    const memoryVariation = formatMemoryVariation(lastUsedMemory, state.memoryTracker.used);
-    const currentMemory = formatBytes(state.memoryTracker.used);
-    let memoryInfoLength = 8 + currentMemory.length;
-    let memoryInfo = `${shortcut}M${white}emory: ${yellow}${currentMemory}`;
-    if (memoryInfoLength + memoryVariation.length < spaceLeftForMemory) {
-      memoryInfo += memoryVariation.formatted;
-      memoryInfoLength += memoryVariation.length;
-    }
-    const memoryDetails = ` (user: ${formatBytes(state.memoryTracker.byType.user)}, strings: ${formatBytes(state.memoryTracker.byType.string)}, system: ${formatBytes(state.memoryTracker.byType.system)})`;
-    if (memoryInfoLength + memoryDetails.length <= spaceLeftForMemory) {
-      memoryInfo += `${white}${memoryDetails}`;
-      memoryInfoLength += memoryDetails.length;
-    }
-    replIO.output(
-      `│${cycleInfo}${border}│${memoryInfo}${''.padStart(spaceLeftForMemory - memoryInfoLength, ' ')}${border}│`
-    );
-
-    const operandsWidth = Math.floor((width - 3) / 2);
-    const callStackWidth = width - 3 - operandsWidth;
-    replIO.output(
-      `${border}├${''.padStart(cycleInfoLength, '─')}┴${''.padStart(operandsWidth - cycleInfoLength - 1, '─')}┬${''.padStart(callStackWidth, '─')}┤`
-    );
-
-    const operandsVariation = formatCountVariation(lastOperandsCount, state.operands.length);
-    let operandsInfo = `${shortcut}O${white}perands: ${yellow}${state.operands.length}`;
-    let operandsInfoSize = 10 + state.operands.length.toString().length;
-    if (operandsInfoSize + operandsVariation.length < operandsWidth) {
-      operandsInfo += operandsVariation.formatted;
-      operandsInfoSize += operandsVariation.length;
-    }
-    replIO.output(`${border}│${operandsInfo}${''.padStart(operandsWidth - operandsInfoSize, ' ')}`);
-
-    const callStackVariation = formatCountVariation(lastCallStackSize, state.calls.length);
-    let callStackInfo = `${white}C${shortcut}a${white}ll stack: ${yellow}${state.calls.length}`;
-    let callStackInfoSize = 12 + state.calls.length.toString().length;
-    let exceptionInfo = '';
-    let exceptionInfoSize = 0;
-    if (state.exception) {
-      exceptionInfo = ` ${red}❌${state.exception}`;
-      exceptionInfoSize = state.exception.length + 2;
-    }
-    if (callStackInfoSize + callStackVariation.length + exceptionInfoSize < callStackWidth) {
-      callStackInfo += callStackVariation.formatted + exceptionInfo;
-      callStackInfoSize += callStackVariation.length + exceptionInfoSize;
-    } else if (callStackInfoSize + exceptionInfoSize < callStackWidth) {
-      callStackInfo += exceptionInfo;
-      callStackInfoSize += exceptionInfoSize;
-    } else if (state.exception && callStackInfoSize + 2 < callStackWidth) {
-      callStackInfo += ' ❌';
-      callStackInfoSize += 2;
-    }
-
-    replIO.output(`${border}│${callStackInfo}${''.padStart(callStackWidth - callStackInfoSize, ' ')}${border}│`);
-
-    const callStack = state.calls;
-    for (let index = 0; index < 5; ++index) {
-      replIO.output(`${border}│${white}`);
-
-      const operand = state.operands.at(index);
-      if (null === operand) {
-        replIO.output(''.padStart(operandsWidth, ' '));
-      } else {
-        const operandInfo = toString(operand, { maxWidth: operandsWidth, includeDebugSource: true });
-        replIO.output(`${colorize(operandInfo)}${''.padStart(operandsWidth - operandInfo.length, ' ')}`);
-      }
-
-      replIO.output(`${border}│${white}`);
-
-      if (index < callStack.length) {
-        const value = state.calls.at(index);
-        const operatorState = state.calls.operatorStateAt(index);
-        const callStackInfo = toString(value, { maxWidth: callStackWidth, operatorState, includeDebugSource: true });
-        const callStackInfoSize = callStackInfo.length;
-        replIO.output(`${colorize(callStackInfo)}${''.padStart(callStackWidth - callStackInfoSize, ' ')}`);
-      } else {
-        replIO.output(''.padStart(callStackWidth, ' '));
-      }
-      replIO.output(`${border}│`);
-    }
-
-    replIO.output(`${border}└${''.padStart(operandsWidth, '─')}┴${''.padStart(callStackWidth, '─')}┘`);
+    const cycleInfoLength = renderCycleAndMemoryInfo({ replIO, state, cycle, width, lastUsedMemory });
+    const { operandsWidth, callStackWidth } = renderOperandAndCallStacksTitle({
+      replIO,
+      state,
+      width,
+      cycleInfoLength,
+      lastOperandsCount,
+      lastCallStackSize
+    });
+    renderOperandAndCallStacks({ replIO, state, operandsWidth, callStackWidth });
 
     replIO.output(`${shortcut}c${white}ontinue ${shortcut}q${white}uit`);
 
