8e3bd77ddc0e0a7c6ffd06730136af978a885c02
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-05-23 23:23:30 -0400

    feat: show memory limit when set

--- repl/options.ts
+++ repl/options.ts
@@ -1,6 +1,7 @@
 import type { StateFactorySettings } from '@psbots/engine';
-import { green, white } from './colors.js';
+import { green, white, yellow } from './colors.js';
 import type { IReplIO } from './IReplIo.js';
+import { formatBytes } from './formatBytes.js';
 
 const checkOption = (options: string[], replIO: IReplIO | undefined, option: string): boolean => {
   if (options.includes(option)) {
@@ -15,6 +16,7 @@ export const buildOptions = (options: string[], replIO?: IReplIO): StateFactoryS
   const maxMemory = options.find((option) => option.startsWith('max-memory='));
   if (maxMemory) {
     maxMemoryBytes = Number.parseInt(maxMemory.split('=')[1]!, 10); // = exists
+    replIO?.output(`${green}💡max-memory set to ${yellow}${formatBytes(maxMemoryBytes)}${white}\r\n`);
   }
   return {
     debugMemory: checkOption(options, replIO, 'debug-memory'),
