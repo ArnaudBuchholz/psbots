9a93a16aa51872827b15e7348195fc8ba4dab04a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-05-28 08:42:52 -0400

    chores: WebComponent interface

---
+++ wi/src/IWebComponent.ts
@@ -0,0 +1,13 @@
+export interface IWebComponent {
+  /** Invoked when the custom element is first connected to the document's DOM */
+  connectedCallback?: () => void
+
+  /** Invoked when the custom element is disconnected from the document's DOM */
+  disconnectedCallback?: () => void
+
+  /** Invoked when the custom element is moved to a new document */
+  adoptedCallback?: () => void
+
+  /** Invoked when one of the custom element's attributes is added, removed, or changed */
+  attributeChangedCallback?: () => void
+}
