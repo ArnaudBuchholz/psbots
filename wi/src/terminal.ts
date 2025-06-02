6293f71e337af3020e3f52834ee7d6f406813a82
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-06-02 08:24:07 -0400

    refactor: reset terminal when options change

--- wi/src/terminal.ts
+++ wi/src/terminal.ts
@@ -5,6 +5,45 @@ import { repl } from '@psbots/repl';
 import type { IWebComponent } from './IWebComponent';
 import styles from './styles.css?raw';
 
+class AbortableReplIO implements IReplIO {
+  private _controller: AbortController = new AbortController();
+  private _aborted = false;
+
+  abort() {
+    this._aborted = true;
+    this._controller.abort();
+  }
+
+  constructor(private _terminal: Terminal) {}
+
+  get abortSignal() {
+    return this._controller.signal;
+  }
+
+  get width() {
+    return this._terminal.cols;
+  }
+
+  get height() {
+    return this._terminal.rows;
+  }
+  
+  input(callback: (data: string) => void) {
+    if (this._aborted) {
+      throw new Error('ReplIO has been aborted');
+    }
+    this._terminal.onData(callback);
+  }
+
+  output(text: string) {
+    if (this._aborted) {
+      console.warn('ReplIO has been aborted, ignoring output:', text);
+    } else {
+      this._terminal.write(text);
+    }
+  }
+}
+
 class PsbotsTerminal extends HTMLElement implements IWebComponent {
   private _root: ShadowRoot;
   private _terminalContainer: HTMLElement;
@@ -40,7 +79,7 @@ class PsbotsTerminal extends HTMLElement implements IWebComponent {
   }
 
   private _terminal: Terminal | null = null;
-  private _lastReplIO: IReplIO | null = null;
+  private _replIO: AbortableReplIO | null = null;
 
   private _reset() {
     if (!this._terminal) {
@@ -50,28 +89,16 @@ class PsbotsTerminal extends HTMLElement implements IWebComponent {
       this._terminal.open(this._terminalContainer);
       fitAddon.fit();
     }
-
-    if (this._lastReplIO) {
-      this._lastReplIO.abort?.abort();
+    if (this._replIO) {
+      this._replIO.abort();
+      this._terminal.write('\u001Bc');
     }
-
-
-    repl({
-      get width() {
-        return this._terminal.cols;
-      },
-      get height() {
-        return this._terminal.rows;
-      },
-      input(callback) {
-        term.onData(callback);
-      },
-      output(text) {
-        term.write(text);
-      }
-    }).catch((error) => {
-      console.error(error);
-    });
+    this._replIO = new AbortableReplIO(this._terminal);
+    const options = this.getAttribute('options')?.split(',') || [];
+    repl(this._replIO, options)
+      .catch((error) => {
+        console.error(error);
+      });
   }
 }
 
