d17d9e53a2239581a87e9008c500e04b115029d3
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-06-20 22:53:35 -0400

    feat: memory & CPU monitor

--- wi/src/main.ts
+++ wi/src/main.ts
@@ -1,4 +1,4 @@
-import { IMemoryByType, IState } from '../../engine/dist/api/index.js';
+import type { IMemoryByType, IState } from '../../engine/dist/api/index.js';
 import './terminal.js';
 
 globalThis.addEventListener('DOMContentLoaded', () => {
@@ -14,15 +14,92 @@ globalThis.addEventListener('DOMContentLoaded', () => {
   const memoryCanvas = memoryMonitor.getContext('2d')!;
   cpuMonitor.setAttribute('width', MONITOR_WIDTH.toString());
   cpuMonitor.setAttribute('height', MONITOR_HEIGHT.toString());
-  // const cpuCanvas = cpuMonitor.getContext('2d');
+  const cpuCanvas = cpuMonitor.getContext('2d')!;
 
-  let lastMonitorTick = Date.now();
   const MONITOR_SAMPLE_RESOLUTION = 10; // number of cycles displayed as in pixel in the monitor
   const MONITOR_SAMPLE_SIZE = MONITOR_SAMPLE_RESOLUTION * MONITOR_WIDTH;
   const memory: IMemoryByType[] = [];
+  const renderMemory = () => {
+    memoryCanvas.clearRect(0, 0, MONITOR_WIDTH, MONITOR_HEIGHT);
+    const aggregatedMemory: IMemoryByType[] = [];
+    let maxAggregatedTotal = 0;
+    for (let index = 0; index < MONITOR_WIDTH && index * MONITOR_SAMPLE_RESOLUTION < memory.length; ++index) {
+      let totalOfString = 0;
+      let totalOfSystem = 0;
+      let totalOfUser = 0;
+      for (
+        let index_ = 0;
+        index_ < MONITOR_SAMPLE_RESOLUTION && index * MONITOR_SAMPLE_RESOLUTION + index_ < memory.length;
+        ++index_
+      ) {
+        const sample = memory[index * MONITOR_SAMPLE_RESOLUTION + index_];
+        totalOfString += sample.string;
+        totalOfSystem += sample.system;
+        totalOfUser += sample.user;
+      }
+      const aggregatedTotal = totalOfString + totalOfSystem + totalOfUser;
+      if (aggregatedTotal > maxAggregatedTotal) {
+        maxAggregatedTotal = aggregatedTotal;
+      }
+      aggregatedMemory.push({
+        string: totalOfString,
+        system: totalOfSystem,
+        user: totalOfUser
+      });
+    }
+    let x = 0;
+    for (const { system, string, user } of aggregatedMemory) {
+      const heightString = (MONITOR_HEIGHT * string) / maxAggregatedTotal;
+      const heightSystem = (MONITOR_HEIGHT * system) / maxAggregatedTotal;
+      const heightUser = (MONITOR_HEIGHT * user) / maxAggregatedTotal;
+      let y = MONITOR_HEIGHT - heightSystem;
+      memoryCanvas.fillStyle = '#E74C3C';
+      memoryCanvas.fillRect(x, y, 1, heightSystem);
+      y -= heightString;
+      memoryCanvas.fillStyle = '#7CB342';
+      memoryCanvas.fillRect(x, y, 1, heightString);
+      y -= heightUser;
+      memoryCanvas.fillStyle = '#4169E1';
+      memoryCanvas.fillRect(x, y, 1, heightUser);
+      ++x;
+    }
+  };
 
+  const cpuCyclesPerSecond: number[] = [];
+  const lastCpuTick = Date.now();
+  let lastCpuCycles = 0;
+  const renderCpu = () => {
+    cpuCanvas.clearRect(0, 0, MONITOR_WIDTH, MONITOR_HEIGHT);
+    let maxCyclesPerSecond = 0;
+    for (const cyclesPerSecond of cpuCyclesPerSecond) {
+      if (cyclesPerSecond > maxCyclesPerSecond) {
+        maxCyclesPerSecond = cyclesPerSecond;
+      }
+    }
+    if (maxCyclesPerSecond === 0) {
+      return; // No data to render
+    }
+    let x = 0;
+    for (const cyclesPerSecond of cpuCyclesPerSecond) {
+      const height = (MONITOR_HEIGHT * cyclesPerSecond) / maxCyclesPerSecond;
+      cpuCanvas.fillStyle = '#F39C12';
+      cpuCanvas.fillRect(x++, MONITOR_HEIGHT - height, 1, height);
+    }
+  };
+
+  let lastMonitorTick = Date.now();
   const monitor = (state: IState, forceRender = false): void | Promise<void> => {
     const now = Date.now();
+    if (lastCpuTick - now > 1000 || forceRender) {
+      if (cpuCyclesPerSecond.length >= MONITOR_WIDTH) {
+        cpuCyclesPerSecond.shift();
+      }
+      cpuCyclesPerSecond.push(lastCpuCycles);
+      cpuMonitor.title = `Cycles: ${lastCpuCycles} / sec`;
+      lastCpuCycles = 0;
+    } else {
+      ++lastCpuCycles;
+    }
     if (memory.length >= MONITOR_SAMPLE_SIZE) {
       memory.shift();
     }
@@ -34,39 +111,18 @@ Total: ${state.memoryTracker.used}
 Peak: ${state.memoryTracker.peak}`;
     memory.push(byType);
     if (now - lastMonitorTick > 100 || forceRender) {
-      memoryCanvas.clearRect(0, 0, MONITOR_WIDTH, MONITOR_HEIGHT);
-      const { peak: maxMemory } = state.memoryTracker;
-      for (let index = 0; index < MONITOR_WIDTH && index * MONITOR_SAMPLE_RESOLUTION < memory.length; ++index) {
-        let totalOfString = 0;
-        let totalOfSystem = 0;
-        let totalOfUser = 0;
-        for (let i = 0; i < MONITOR_SAMPLE_RESOLUTION && index * MONITOR_SAMPLE_RESOLUTION + i < memory.length; ++i) {
-          const sample = memory[index * MONITOR_SAMPLE_RESOLUTION + i];
-          totalOfString += sample.string;
-          totalOfSystem += sample.system;
-          totalOfUser += sample.user;
-        }
-        const total = totalOfString + totalOfSystem + totalOfUser;
-        const height = (MONITOR_HEIGHT * total) / (maxMemory * MONITOR_SAMPLE_RESOLUTION);
-        const heightString = (MONITOR_HEIGHT * totalOfString) / (maxMemory * MONITOR_SAMPLE_RESOLUTION);
-        const heightSystem = (MONITOR_HEIGHT * totalOfSystem) / (maxMemory * MONITOR_SAMPLE_RESOLUTION);
-        const heightUser = (MONITOR_HEIGHT * totalOfUser) / (maxMemory * MONITOR_SAMPLE_RESOLUTION);
-        memoryCanvas.fillStyle = '#f00';
-        memoryCanvas.fillRect(index, MONITOR_HEIGHT - height,  1, heightSystem);
-        memoryCanvas.fillStyle = '#00f';
-        memoryCanvas.fillRect(index, MONITOR_HEIGHT - height + heightSystem,  1, heightString);
-        memoryCanvas.fillStyle = '#0f0';
-        memoryCanvas.fillRect(index, MONITOR_HEIGHT - height + heightSystem + heightString,  1, heightUser);
-      }
+      renderMemory();
+      renderCpu();
       lastMonitorTick = now;
-      return new Promise<void>((resolve) => setTimeout(resolve, 0))
+      return new Promise<void>((resolve) => setTimeout(resolve, 0));
     }
-  }
+  };
 
   terminal.addEventListener('start', (event) => {
     const terminalEvent = event as CustomEvent;
     memory.length = 0;
-    terminalEvent.detail.wait = monitor(terminalEvent.detail.state)
+    cpuCyclesPerSecond.length = 0;
+    terminalEvent.detail.wait = monitor(terminalEvent.detail.state);
     status.innerHTML = '⭕';
   });
 
@@ -95,7 +151,7 @@ Peak: ${state.memoryTracker.peak}`;
 
   terminal.addEventListener('cycle', (event) => {
     const terminalEvent = event as CustomEvent;
-    terminalEvent.detail.wait = monitor(terminalEvent.detail.state)
+    terminalEvent.detail.wait = monitor(terminalEvent.detail.state);
     status.innerHTML = '🟡';
   });
 
