24a3d7fddeb4c7a50281ea9eda9655debcc50718
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-06-25 00:08:42 -0400

    feat: optimize build (wip)

--- tools/postBuild.mjs
+++ tools/postBuild.mjs
@@ -1,6 +1,13 @@
 /* eslint-env node */
 import { readdir, readFile, stat, writeFile } from 'node:fs/promises';
 import { join, relative } from 'node:path';
+import { parse } from '@babel/parser';
+import { generate } from '@babel/generator';
+import esquery from 'esquery';
+
+const tsconfig = JSON.parse(await readFile('tsconfig.json'));
+const aliases = tsconfig.compilerOptions.paths;
+const basePath = process.argv[2];
 
 async function removeAliases(basePath, path = basePath) {
   const names = await readdir(path);
@@ -31,8 +38,64 @@ async function removeAliases(basePath, path = basePath) {
     }
   }
 }
-
-const tsconfig = JSON.parse(await readFile('tsconfig.json'));
-const aliases = tsconfig.compilerOptions.paths;
-const basePath = process.argv[2];
 await removeAliases(basePath);
+
+const assertCallExpression = esquery.parse('ExpressionStatement[expression.callee.name=assert]');
+
+function forEachMatch(ast, matches, callback/*parent, member, match*/) {
+  function traverseArray(array) {
+    for (let i = array.length - 1; i >= 0; i--) {
+      if (matches.includes(array[i])) {
+        callback(array, i, array[i]);
+      } else {
+        traverse(array[i]);
+      }
+    }
+  }
+
+  function traverse(node) {
+    for (const key in node) {
+      value = node[key];
+      if (Array.isArray(value)) {
+        traverseArray(value);
+      } else if (typeof value === 'object' && value) {
+        if (matches.includes(value)) {
+          callback(node, key, value);
+        } else {
+          traverse(value);
+        }
+      }
+    }
+  }
+
+  if (matches.length) {
+    traverse(ast);
+  }
+}
+
+async function removeAsserts(basePath, path = basePath) {
+  const names = await readdir(path);
+  for (const name of names) {
+    const itemPath = join(path, name);
+    if (name.endsWith('.js') && !name.endsWith('.ast.js')) {
+      const source = await readFile(itemPath, 'utf8');
+      const ast = parse(source, { sourceType: 'module' });
+      await writeFile(itemPath.replace('.js', '.ast'), JSON.stringify(ast, null, 2), { encoding: 'utf8' });
+
+      forEachMatch(ast, assertCallExpression, (parent, member, match) => {
+        if (!Array.isArray(parent)) {
+          throw new Error('Unexpected');
+        }
+        parent.splice(member, 1);
+      });
+
+      await writeFile(itemPath.replace('.js', '.ast.js'), generate(ast).code, { encoding: 'utf8' });
+    } else {
+      const itemStat = await stat(itemPath);
+      if (itemStat.isDirectory()) {
+        await removeAsserts(basePath, itemPath);
+      }
+    }
+  }
+}
+await removeAsserts(basePath);
