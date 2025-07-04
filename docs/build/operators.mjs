15a1d84c13517dce2e75e432913f49ae933abea5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-07-04 00:00:55 -0400

    fix: linter

--- docs/build/operators.mjs
+++ docs/build/operators.mjs
@@ -2,9 +2,7 @@ import { readFile, writeFile } from 'node:fs/promises';
 import { getOperatorDefinitionRegistry } from '../../engine/dist/index.js';
 import { write } from 'node:fs';
 
-const ref2 = JSON.parse(
-  await readFile(new URL('../operators/ref2.json', import.meta.url), 'utf8')
-);
+const reference2 = JSON.parse(await readFile(new URL('../operators/ref2.json', import.meta.url), 'utf8'));
 
 const registry = getOperatorDefinitionRegistry();
 
@@ -18,55 +16,39 @@ const registry = getOperatorDefinitionRegistry();
 |📄| device API (not part of engine) |
 */
 
-for (const [name, label] of Object.entries(ref2)) {
-  const tags = {
-    '⚙️': ['repl'],
-    '❓': ['unknown'],
-    '❌': ['not-implemented'],
-    '📄': ['device'],
-  }[label] ?? [];
+for (const [name, label] of Object.entries(reference2)) {
+  const tags =
+    {
+      '⚙️': ['repl'],
+      '❓': ['unknown'],
+      '❌': ['not-implemented'],
+      '📄': ['device']
+    }[label] ?? [];
   const markdown = [
     '---',
     'tags:',
     '  - operator',
     ...tags.map((tag) => `  - ${tag}`),
-    '---' ,
-    `[Postscript Level 2 documentation](https://hepunx.rl.ac.uk/~adye/psdocs/ref/PSL2${name.charAt(0)}.html#${name})`,
-  ]
+    '---',
+    `[Postscript Level 2 documentation](https://hepunx.rl.ac.uk/~adye/psdocs/ref/PSL2${name.charAt(0)}.html#${name})`
+  ];
   const definition = registry[name];
   if (definition) {
-    markdown.push(
-      `🏷️ ${ definition.labels.map(label => '[[' + label + ']]').join(' ') }`,
-      definition.description
-    );
+    markdown.push(`🏷️ ${definition.labels.map((label) => '[[' + label + ']]').join(' ')}`, definition.description);
   }
-  await writeFile(
-    new URL(`../operators/${name}.md`, import.meta.url),
-    markdown.join('\n'),
-    'utf8'
-  );
+  await writeFile(new URL(`../operators/${name}.md`, import.meta.url), markdown.join('\n'), 'utf8');
 }
 
 // extensions (operators defined in registry but not in ref2.json)
-const extensions = Object.keys(registry).filter((name) => name.match(/\w+/) && !ref2.hasOwnProperty(name));
+const extensions = Object.keys(registry).filter((name) => name.match(/\w+/) && !reference2.hasOwnProperty(name));
 for (const name of extensions) {
-  const markdown = [
-    '---',
-    'tags:',
-    '  - operator',
-    '  - extension',
-    '---' ,
-  ]
+  const markdown = ['---', 'tags:', '  - operator', '  - extension', '---'];
   const definition = registry[name];
   if (definition) {
     markdown.push(
-      `🏷️ [[extension]] ${ definition.labels.map(label => '[[' + label + ']]').join(' ') }`,
+      `🏷️ [[extension]] ${definition.labels.map((label) => '[[' + label + ']]').join(' ')}`,
       definition.description
     );
   }
-  await writeFile(
-    new URL(`../operators/${name}.md`, import.meta.url),
-    markdown.join('\n'),
-    'utf8'
-  );
+  await writeFile(new URL(`../operators/${name}.md`, import.meta.url), markdown.join('\n'), 'utf8');
 }
