76d2da242176fc5950a13f71e41df6598e352267
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-07-07 23:54:46 -0400

    feat: generate analysis

--- docs/build/engineSources.mjs
+++ docs/build/engineSources.mjs
@@ -4,8 +4,9 @@ import { parse } from '@babel/parser';
 import _traverse from '@babel/traverse';
 const traverse = _traverse.default;
 
-const sources = {};
-const exportedFunctions = {};
+let lastId = 0;
+const sources = Object.create(null);
+const exportedFunctions = Object.create(null);
 
 const red = '\u001B[31m';
 const yellow = '\u001B[33m';
@@ -33,12 +34,13 @@ const searchForCalls = (scope, node) => {
 };
 
 const source = async (path) => {
-  const name = path.pathname.split('engine/src/')[1];
+  const moduleName = path.pathname.split('engine/src/')[1];
   const content = await readFile(path, 'utf8');
   const ast = parse(content, { sourceType: 'module', plugins: ['typescript'] });
   await writeFile(new URL(path.toString().replace('.ts', '.ast')), JSON.stringify(ast, null, 2), { encoding: 'utf8' });
   const definition = {
-    name,
+    id: ++lastId,
+    name: moduleName,
     imports: [],
     calls: new Map(),
     classes: [],
@@ -80,14 +82,20 @@ const source = async (path) => {
         const name = node.id.name;
         const exported = parent.type === 'ExportNamedDeclaration';
         const functionDefinition = {
+          id: ++lastId,
+          module: moduleName,
           name,
           exported,
-          calls: searchForCalls(scope, node)
+          calls: searchForCalls(scope, node),
+          externalCalls: 0
         };
         definition.functions.push(functionDefinition);
         if (exported) {
-          if (exportedFunctions[name]) {
-            throw new Error(`❌ duplicate exported function name: ${name}`);
+          const existingFunction = exportedFunctions[name];
+          if (existingFunction) {
+            throw new Error(`❌ duplicate exported function name:
+${name} from ${moduleName}
+${existingFunction.name} from ${existingFunction.module}`);
           }
           exportedFunctions[name] = functionDefinition;
         }
@@ -95,6 +103,7 @@ const source = async (path) => {
       }
       if (node.type === 'ArrowFunctionExpression') {
         definition.functions.push({
+          id: ++lastId,
           name: parent.id?.name ?? '(anonymous arrow)',
           exported: false, // TODO ?
           calls: searchForCalls(scope, node)
@@ -103,7 +112,7 @@ const source = async (path) => {
       }
     }
   });
-  sources[name] = definition;
+  sources[moduleName] = definition;
 };
 
 const lookup = async (path) => {
@@ -126,30 +135,87 @@ const names = Object.keys(sources).sort((a, b) => a.localeCompare(b));
 
 for(const name of names) {
   const definition = sources[name];
-  console.log(`${yellow}${name}${white}`);
-  for (const { name, from } of definition.imports) {
-    console.log(`\t← ${name} (${from})`);
-  }
   for (const [name, { count }] of definition.calls.entries()) {
-    console.log(`\t→ ${name}${count > 1 ? ' (' + count + ')' : ''}`);
+    const functionDefinition = exportedFunctions[name];
+    if (functionDefinition) {
+      functionDefinition.externalCalls += count;
+    }
   }
-  for (const { name: className, exported, methods } of definition.classes) {
-    console.log(`\t${exported ? red + 'export' + white + ' ' : ''}class ${className}`);
-    for (const { name: methodName, calls } of methods) {
-      if (calls.size) {
-        console.log(`\t${className}::${methodName}`);
-        for (const [name, { count }] of calls.entries()) {
-          console.log(`\t  → ${name}${count > 1 ? ' (' + count + ')' : ''}`);
+  for (const { methods } of definition.classes) {
+    for (const { calls } of methods) {
+      for (const [name, { count }] of calls.entries()) {
+        const functionDefinition = exportedFunctions[name];
+        if (functionDefinition) {
+          functionDefinition.externalCalls += count;
         }
       }
     }
   }
-  for (const { name: functionName, exported, calls } of definition.functions) {
-    if (exported || calls.size) {
-      console.log(`\t${exported ? red + 'export' + white + ' ' : ''}function ${functionName}`);
-      for (const [name, { count }] of calls.entries()) {
-          console.log(`\t  → ${name}${count > 1 ? ' (' + count + ')' : ''}`);
+  for (const { calls } of definition.functions) {
+    for (const [name, { count }] of calls.entries()) {
+      const functionDefinition = exportedFunctions[name];
+      if (functionDefinition) {
+        functionDefinition.externalCalls += count;
       }
     }
   }
 }
+
+const markdown = [
+  '# Sources',
+  '## Dependencies',
+];
+
+for(const name of names) {
+  const definition = sources[name];
+  console.log(`${yellow}${name}${white}`);
+  markdown.push(`### ${name}`);
+  for (const { name, from } of definition.imports) {
+    console.log(`\t← ${name} (${from})`);
+    markdown.push(`\n* ← ${name} (${from})`);
+  }
+  for (const [name, { count }] of definition.calls.entries()) {
+    console.log(`\t→ ${name}${count > 1 ? ' (' + count + ')' : ''}`);
+    markdown.push(`\n* → ${name}${count > 1 ? ' (' + count + ')' : ''}`);
+  }
+  for (const { name: className, exported, methods } of definition.classes) {
+    console.log(`\t${exported ? red + 'export' + white + ' ' : ''}class ${className}`);
+    markdown.push(`\n${exported ? '<span style="color: red;">export</span> ' : ''}class ${className}`);
+    for (const { name: methodName, calls } of methods) {
+      if (calls.size) {
+        console.log(`\t${className}::${methodName}`);
+        markdown.push(`  ${className}::${methodName}`);
+        for (const [name, { count }] of calls.entries()) {
+          console.log(`\t  → ${name}${count > 1 ? ' (' + count + ')' : ''}`);
+          markdown.push(`\n*  → ${name}${count > 1 ? ' (' + count + ')' : ''}`);
+        }
+      }
+    }
+  }
+  for (const { name: functionName, exported, calls, externalCalls } of definition.functions) {
+    if (exported || calls.size) {
+      console.log(`\t${exported ? red + 'export' + white + ' ' : ''}function ${functionName} ${ externalCalls ? '(' + externalCalls + ')' : '' }`);
+      markdown.push(`\n${exported ? '<span style="color: red;">export</span> ' : ''}function ${functionName} ${ externalCalls ? '(' + externalCalls + ')' : '' }`);
+      for (const [name, { count }] of calls.entries()) {
+          console.log(`\t  → ${name}${count > 1 ? ' (' + count + ')' : ''}`);
+          markdown.push(`\n*  → ${name}${count > 1 ? ' (' + count + ')' : ''}`);
+      }
+    }
+  }
+}
+
+markdown.push(
+  '## Grap',
+  '```mermaid',
+  'graph LR'
+);
+for(const name of names) {
+  const definition = sources[name];
+  markdown.push(`  subgraph ${name}`);
+  for (const [name, { count }] of definition.calls.entries()) {
+    markdown.push(`    main_${definition.id}("main") --> ${name};`);
+  }
+  markdown.push(`  end`);
+}
+markdown.push('```');
+await writeFile(new URL('../engine/sources.md', import.meta.url), markdown.join('\n'), 'utf8')
