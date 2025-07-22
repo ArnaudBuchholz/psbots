a24c3fbadeb2554980284c486096bf6630c339d6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-07-21 22:16:46 -0400

    refactor: isolate optimization part

--- tools/postBuild.mjs
+++ tools/postBuild.mjs
@@ -1,10 +1,6 @@
 /* eslint-env node */
 import { readdir, readFile, stat, writeFile, mkdir } from 'node:fs/promises';
 import { join, relative } from 'node:path';
-import { parse } from '@babel/parser';
-import { generate } from '@babel/generator';
-import _traverse from '@babel/traverse';
-const traverse = _traverse.default;
 
 const tsconfig = JSON.parse(await readFile('tsconfig.json'));
 const aliases = tsconfig.compilerOptions.paths;
@@ -40,39 +36,3 @@ async function removeAliases(basePath, path = basePath) {
   }
 }
 await removeAliases(basePath);
-
-async function optimize(basePath, path = basePath) {
-  if (/\bperf\b/.test(path)) {
-    return;
-  }
-  const names = await readdir(path);
-  const perfPath = path.replace(/\bdist\b/, 'dist/perf');
-  await mkdir(perfPath, { recursive: true });
-  for (const name of names) {
-    const itemPath = join(path, name);
-    if (name.endsWith('.js')) {
-      const source = await readFile(itemPath, 'utf8');
-      const ast = parse(source, { sourceType: 'module' });
-      await writeFile(join(perfPath, name.replace('.js', '.ast')), JSON.stringify(ast, null, 2), { encoding: 'utf8' });
-
-      // Remove calls to assert
-      traverse(ast, {
-        enter(path) {
-          // TODO: check how assert is imported and remove statement
-          const { node } = path;
-          if (node.type === 'ExpressionStatement' && node.expression?.callee?.name === 'assert') {
-            path.remove();
-          }
-        }
-      });
-
-      await writeFile(join(perfPath, name), generate(ast).code, { encoding: 'utf8' });
-    } else {
-      const itemStat = await stat(itemPath);
-      if (itemStat.isDirectory()) {
-        await optimize(basePath, itemPath);
-      }
-    }
-  }
-}
-await optimize(basePath);
