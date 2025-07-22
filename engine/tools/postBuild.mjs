1234b53617e705c86df6bbdd80b22875911f43ef
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-07-21 23:02:30 -0400

    feat: look for inlining toIntegerValue

--- engine/tools/postBuild.mjs
+++ engine/tools/postBuild.mjs
@@ -8,6 +8,24 @@ const traverse = _traverse.default;
 
 const basePath = process.argv[2];
 
+const removeImport = (path, importedName) => {
+  const { node } = path;
+  if (node.type === 'ImportDeclaration') {
+    // Assuming name has not been changed and from is not relevant
+    const { specifiers } = node;
+    const assertIndex = specifiers.findIndex((specifier) => specifier.imported.name === importedName);
+    if (assertIndex !== -1) {
+      if (specifiers.length === 1) {
+        path.remove();
+      } else {
+        specifiers.splice(assertIndex, 1);
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
 async function optimize(basePath, path = basePath) {
   if (/\bperf\b/.test(path)) {
     return;
@@ -22,21 +40,46 @@ async function optimize(basePath, path = basePath) {
       const ast = parse(source, { sourceType: 'module' });
       await writeFile(join(perfPath, name.replace('.js', '.ast')), JSON.stringify(ast, null, 2), { encoding: 'utf8' });
 
+      // Search for valid replacements of toIntegerValue
+      let totalCount = 0;
+      let removableCount = 0;
+      traverse(ast, {
+        enter(path) {
+          const { node } = path;
+          if (node.type === 'VariableDeclaration' && path.toString().includes('toIntegerValue')) {
+            ++totalCount;
+            const [, variableName] = path.toString().match(/\b(\w+)\s+=\s+toIntegerValue/);
+            if (path.getNextSibling().toString().startsWith(`assert(${variableName}`)) {
+              ++removableCount;
+            }
+          }
+        }
+      });
+      if (removableCount > 0) {
+        traverse(ast, {
+          enter(path) {
+            const { node } = path;
+            if (removableCount === totalCount && removeImport(path, 'toIntegerValue')) {
+              path.skip();
+              return;
+            }
+            if (node.type === 'VariableDeclaration' && path.toString().includes('toIntegerValue')) {
+              const [, variableName] = path.toString().match(/\b(\w+)\s+=\s+toIntegerValue/);
+              if (path.getNextSibling().toString().startsWith(`assert(${variableName}`)) {
+                // replace with inline
+              }
+            }
+          }
+        });
+      }
+
       // Remove calls to assert
       traverse(ast, {
         enter(path) {
           const { node } = path;
-          if (node.type === 'ImportDeclaration') {
-            // Assuming name has not been changed and from is not relevant
-            const { specifiers } = node;
-            const assertIndex = specifiers.findIndex((specifier) => specifier.imported.name === 'assert');
-            if (assertIndex !== -1) {
-              if (specifiers.length === 1) {
-                path.remove();
-              } else {
-                specifiers.splice(assertIndex, 1);
-              }
-            }
+          if (removeImport(path, 'assert')) {
+            path.skip();
+            return;
           }
           if (node.type === 'ExpressionStatement' && node.expression?.callee?.name === 'assert') {
             path.remove();
@@ -44,7 +87,9 @@ async function optimize(basePath, path = basePath) {
         }
       });
 
-      await writeFile(join(perfPath, name.replace('.js', '.optimized.ast')), JSON.stringify(ast, null, 2), { encoding: 'utf8' });
+      await writeFile(join(perfPath, name.replace('.js', '.optimized.ast')), JSON.stringify(ast, null, 2), {
+        encoding: 'utf8'
+      });
       await writeFile(join(perfPath, name), generate(ast).code, { encoding: 'utf8' });
     } else {
       const itemStat = await stat(itemPath);
