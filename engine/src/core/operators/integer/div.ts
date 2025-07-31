6acc43fd6b4a42b9378c02a41eabd95d620b90d2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-07-31 07:23:49 -0400

    fix: remove useless tests

--- engine/src/core/operators/integer/div.ts
+++ engine/src/core/operators/integer/div.ts
@@ -1,6 +1,9 @@
 import { assert, toIntegerValue } from '@sdk/index.js';
 import { buildFunctionOperator } from '@core/operators/operators.js';
 
+assert(Number.MAX_SAFE_INTEGER / -1 === Number.MIN_SAFE_INTEGER);
+assert(Number.MIN_SAFE_INTEGER / -1 === Number.MAX_SAFE_INTEGER);
+
 buildFunctionOperator(
   {
     name: 'div',
