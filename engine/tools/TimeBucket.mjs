156050a698b9eca307d7ee63201b5db63757b391
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-08-13 23:19:15 -0400

    feat: improve aberrant values filtering (z-score with 2*stdev)

--- engine/tools/TimeBucket.mjs
+++ engine/tools/TimeBucket.mjs
@@ -35,10 +35,10 @@ export class TimeBucket {
     return 10 ** powerOf10;
   }
 
-  #resolution
-  #count
-  #ranges
-  #maxHits
+  #resolution;
+  #count;
+  #ranges;
+  #maxHits;
 
   constructor(resolution = TimeBucket.getResolution()) {
     this.#resolution = resolution;
@@ -54,37 +54,58 @@ export class TimeBucket {
   /** duration is expressed in ms (based on performance.now()) */
   add(duration) {
     ++this.#count;
-    const index = Math.floor(duration * TimeBucket.SCALE / this.#resolution);
+    const index = Math.floor((duration * TimeBucket.SCALE) / this.#resolution);
     const rangeHits = (this.#ranges[index] ?? 0) + 1;
     this.#ranges[index] = rangeHits;
     this.#maxHits = Math.max(rangeHits, this.#maxHits);
   }
 
   /** returns { count, min, max, mean } when enough data (undefined otherwise) */
-  cleanedStats() {
-    const threshold = Math.ceil(this.#count / 100);
+  cleanedStats(options = {}) {
+    const { thresholdType = 'z-score', msUnitFactor = 1 } = options;
     let min = Number.POSITIVE_INFINITY;
     let max = 0;
     let sum = 0;
-    let count = 0;
+    let hitsTotal = 0;
     for (const index of Object.keys(this.#ranges)) {
-      const value = this.#ranges[index];
-      const duration = parseInt(index);
-      if (value < threshold) {
-        continue; // ignore
+      const hits = this.#ranges[index];
+      const duration = (Number.parseInt(index) * this.#resolution * msUnitFactor) / TimeBucket.SCALE;
+      sum += hits * duration;
+      hitsTotal += hits;
+    }
+    let mean = sum / hitsTotal;
+    let count = 0;
+    if (thresholdType === 'z-score') {
+      let sumOfVariances = 0;
+      for (const index of Object.keys(this.#ranges)) {
+        const hits = this.#ranges[index];
+        const duration = (Number.parseInt(index) * this.#resolution * msUnitFactor) / TimeBucket.SCALE;
+        const diff = duration - mean;
+        const variance = diff * diff;
+        sumOfVariances += hits * variance;
       }
-      min = Math.min(min, duration);
-      max = Math.max(max, duration);
-      sum += value * duration;
-      ++count;
+      const standardDeviation = Math.sqrt(sumOfVariances / hitsTotal);
+      sum = 0;
+      for (const index of Object.keys(this.#ranges)) {
+        const duration = (Number.parseInt(index) * this.#resolution * msUnitFactor) / TimeBucket.SCALE;
+        if (duration < mean - 2 * standardDeviation || duration > mean + 2 * standardDeviation) {
+          continue; // ignore
+        }
+        min = Math.min(min, duration);
+        max = Math.max(max, duration);
+        const hits = this.#ranges[index];
+        sum += hits * duration;
+        count += hits;
+      }
     }
     if (count !== 0) {
       return {
         count,
         min,
         max,
-        mean: Math.floor(sum / count)
+        mean: sum / count,
+        ignored: hitsTotal - count
       };
     }
   }
-};
+}
